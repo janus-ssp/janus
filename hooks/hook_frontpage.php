9f49aa81879b1de78df806362dadd081bfc00a42
Jacob Christiansen <jacob@colourbox.com>
2009-08-04 11:51:29 +0000

    Fixed typo

--- hooks/hook_frontpage.php
+++ hooks/hook_frontpage.php
@@ -3,14 +3,14 @@
  * File containing a frontpage hook
  *
  * @author Jacob Christiaansen, <jach@wayf.dk>
- * @package simpleSMALphp
+ * @package simpleSAMLphp
  * @subpackage JANUS
  * @varsion $Id$
  */
 /**
  * Hook to add the JANUS module to the frontpage.
  *
- * @param array &$links  The links on the frontpage, split into sections.
+ * @param array &$links The links on the frontpage, split into sections.
  */
 function janus_hook_frontpage(&$links) {
 	assert('is_array($links)');
