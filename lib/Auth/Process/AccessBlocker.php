8bc897aa8107079bda9412e234dd419216acdb90
Jacob Christiansen <jacob@colourbox.com>
2009-08-05 12:55:28 +0000

    Added PEAR coding style standard and added LGPL license to comment

--- lib/Auth/Process/AccessBlocker.php
+++ lib/Auth/Process/AccessBlocker.php
@@ -1,95 +1,137 @@
 <?php
 /**
- * AccessBlocker processing filter for simpleSAMLphp
+ * An AccessBlocker processing filter
  *
- * @author Jacob Christiansen, <jach@wayf.dk>
- * @package simpleSAMLphp
- * @subpackage JANUS
- * @version $Id$
+ * PHP version 5
+ *
+ * JANUS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * JANUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JANUS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category   SimpleSAMLphp
+ * @package    JANUS
+ * @subpackage ProcessingFilter
+ * @author     Jacob Christiansen <jach@wayf.dk>
+ * @copyright  2009 Jacob Christiansen 
+ * @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @version    SVN: $Id$
+ * @link       http://code.google.com/p/janus-ssp/
  */
 /**
- * Filter to block access to entities..
+ * An AccessBlocker processing filter
  *
  * This filter is a basic implementation of an access blocking processing 
  * filter for simpleSAMLphp. The implementation is user in JANUS. The filter is 
  * configured in either the global config file or in the metadata files for the 
- * entities. 
+ * entity. 
  * <code>
  * 10 => array(
- * 	'class' => 'janus:AccessBlocker',
- * 		'blocked' => array(
- *			'entity1',
- *			'entity2',
- *			...
- * 		),
- * 	),
+ *    'class' => 'janus:AccessBlocker',
+ *    'blocked' => array(
+ *       'entity1',
+ *       'entity2',
+ *       ...
+ *    ),
+ * ),
  * </code>
+ * 
+ * The filter will block access to the entities given in the configuration.
  *
- * @package simpleSAMLphp
- * @subpackage JANUS
+ * @category   SimpleSAMLphp
+ * @package    JANUS
+ * @subpackage ProcessingFilter
+ * @author     Jacob Christiansen <jach@wayf.dk>
+ * @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @link       http://code.google.com/p/janus-ssp/
+ * @see        SimpleSAML_Auth_ProcessingFilter
+ * @since      Release 1.0.0
  */
-class sspmod_janus_Auth_Process_AccessBlocker extends SimpleSAML_Auth_ProcessingFilter {
-	
-	/**
-	 * Array of blocked entities.
-	 * @var array
-	 */
-	private $blocked = array();
-	
-	/**
-	 * Initialize this filter.
-	 *
-	 * @param array $config Configuration information about this filter.
-	 * @param mixed $reserved For future use.
-	 */
-	public function __construct($config, $reserved) {
-		assert('is_array($config)');
-		
-		// Call parent constructor
-		parent::__construct($config, $reserved);
+class Sspmod_Janus_Auth_Process_AccessBlocker extends SimpleSAML_Auth_ProcessingFilter
+{
+    /**
+     * Array of blocked entities
+     * @var array
+     */
+    private $_blocked = array();
 
-		// Process config array
-		foreach($config AS $name => $value) {
-			if(!is_string($name)) {
-				throw new SimpleSAML_Error_Exception('Config parameter must be string in janus:AccessBlocker: ' . var_export($name, TRUE));
-			}
+    /**
+     * Initialize this filter
+     *
+     * @param array $config   The configuration information about this filter
+     * @param mixed $reserved For future use
+     *
+     * @throws SimpleSAML_Error_Exception
+     * @since Release 1.0.0
+     */
+    public function __construct($config, $reserved)
+    {
+        assert('is_array($config)');
 
-			// If parameter is `blocked`
-			if($name === 'blocked') {
-				if(!is_array($value) && is_string($value)) {
-					$this->blocked = array($value);
-				} else {
-					$this->blocked = $value;
-				}
-			} else {
-				new SimpleSAML_Error_Exception('Invalid config parameter given to janus:AccessBlocker: ' . var_export($name, TRUE));
-			}
-		}
-	}
+        // Call parent constructor
+        parent::__construct($config, $reserved);
 
-	/**
-	 * Apply filter to block entities.
-	 *
-	 * Block the entity making the request if it is set in the configuration.
-	 *
-	 * @param array &$state The current state.
-	 */
-	public function process(&$state) {
-		assert('is_array($state)');
+        // Process config array
+        foreach ($config AS $name => $value) {
+            if (!is_string($name)) {
+                throw new SimpleSAML_Error_Exception(
+                    'Config parameter must be string in janus:AccessBlocker: ' 
+                    . var_export($name, true));
+            }
 
-		$session = SimpleSAML_Session::getInstance();
+            // If parameter is `blocked`
+            if ($name === 'blocked') {
+                if (!is_array($value) && is_string($value)) {
+                    $this->_blocked = array($value);
+                } else {
+                    $this->_blocked = $value;
+                }
+            } else {
+                new SimpleSAML_Error_Exception(
+                    'Invalid config parameter given to janus:AccessBlocker: ' 
+                    . var_export($name, true));
+            }
+        }
+    }
 
-		// Get the IdP
-		$remote_entity_idp = $session->getIdP();
-		// Get the SP
-		$remote_entity_sp = $state['Destination']['entityid'];
-	
-		if(in_array($remote_entity_sp, $this->blocked, TRUE) || in_array($remote_entity_idp, $this->blocked, TRUE)) {
-			// IdP or SP should be blocked. Save the state and redirect	
-			$id = SimpleSAML_Auth_State::saveState($state, 'janus:accessblock');
-			$url = SimpleSAML_Module::getModuleURL('janus/showaccessblock.php');
-			SimpleSAML_Utilities::redirect($url, array('StateId' => $id));
-		}
-	}
+    /**
+     * Apply filter to block entities
+     *
+     * Stop the current authentication request if either the Identity Provider
+     *  or the Service Provider is set to be blocked by the configuration.
+     *
+     * @param array &$state The current state
+     *
+     * @return void 
+     * @since Release 1.0.0
+     */
+    public function process(&$state)
+    {
+        assert('is_array($state)');
+
+        $session = SimpleSAML_Session::getInstance();
+
+        // Get the IdP
+        $remote_entity_idp = $session->getIdP();
+        // Get the SP
+        $remote_entity_sp = $state['Destination']['entityid'];
+
+        if (   in_array($remote_entity_sp, $this->_blocked, true) 
+            || in_array($remote_entity_idp, $this->_blocked, true)
+        ) {
+            // IdP or SP should be blocked. Save the state and redirect	
+            $id = SimpleSAML_Auth_State::saveState($state, 'janus:accessblock');
+            $url = SimpleSAML_Module::getModuleURL('janus/showaccessblock.php');
+            SimpleSAML_Utilities::redirect($url, array('StateId' => $id));
+        }
+    }
 }
 ?>
