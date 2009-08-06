00245b56904e7268c576512d8857c659b596caeb
Jacob Christiansen <jacob@colourbox.com>
2009-08-06 09:48:37 +0000

    Updated documentation

--- hooks/hook_frontpage.php
+++ hooks/hook_frontpage.php
@@ -1,25 +1,51 @@
 <?php
 /**
- * File containing a frontpage hook
+ * Frontpage hook for JANUS
  *
- * @author Jacob Christiaansen, <jach@wayf.dk>
- * @package simpleSAMLphp
- * @subpackage JANUS
- * @varsion $Id$
+ * PHP version 5
+ *
+ * JANUS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * JANUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JANUS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category   SimpleSAMLphp
+ * @package    JANUS
+ * @subpackage Hooks
+ * @author     Jacob Christiansen <jach@wayf.dk>
+ * @copyright  2009 Jacob Christiansen 
+ * @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @version    SVN: $Id$
+ * @link       http://code.google.com/p/janus-ssp/
  */
 /**
- * Hook to add the JANUS module to the frontpage.
+ * Frontpage hook for JANUS
  *
- * @param array &$links The links on the frontpage, split into sections.
+ * This hook adds a link to JANUS to the frontapage of the local SimpleSAMLphp 
+ * installation. 
+ *
+ * @param array &$links The links on the frontpage, split into sections
+ *
+ * @return void
+ *
+ * @since      Release 1.0.0
  */
-function janus_hook_frontpage(&$links) {
-	assert('is_array($links)');
-	assert('array_key_exists("links", $links)');
+function Janus_Hook_frontpage(&$links)
+{
+    assert('is_array($links)');
+    assert('array_key_exists("links", $links)');
 
-	$links['links'][] = array(
-		'href' => SimpleSAML_Module::getModuleURL('janus/index.php'),
-		'text' => array('en' => 'JANUS module'),
-	);
-
+    $links['links'][] = array(
+        'href' => SimpleSAML_Module::getModuleURL('janus/index.php'),
+        'text' => array('en' => 'JANUS module'),
+    );
 }
 ?>
