c4f1d0622c9a97ab0f57db7b7abc41c63f177ea5
Jacob Christiansen <jacob@colourbox.com>
2009-08-12 13:06:11 +0000

    Updated infil edocumentation

--- lib/User.php
+++ lib/User.php
@@ -1,300 +1,429 @@
 <?php
 /**
- * Contains User class for JANUS.
+ * A user
  *
- * @author Jacob Christiansen, <jach@wayf.dk>
- * @package simpleSAMLphp
- * @subpackage JANUS
- * @version $Id$
+ * PHP version 5
+ *
+ * JANUS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * JANUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JANUS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category   SimpleSAMLphp
+ * @package    JANUS
+ * @subpackage Core
+ * @author     Jacob Christiansen <jach@wayf.dk>
+ * @copyright  2009 Jacob Christiansen 
+ * @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @version    SVN: $Id$
+ * @link       http://code.google.com/p/janus-ssp/
+ * @since      File available since Release 1.0.0
  */
 /**
- * Class implementing a JANUS user.
+ * A user
  *
- * User class that extends the Database class implementing basic functionality
- * used for user generation and updating.
+ * Basic implementation of a user. NOTE that the way extra data regarding the 
+ * user is stored will change in the future.
  *
- * @package simpleSAMLphp
- * @subpackage JANUS
+ * @category   SimpleSAMLphp
+ * @package    JANUS
+ * @subpackage Core
+ * @author     Jacob Christiansen <jach@wayf.dk>
+ * @copyright  2009 Jacob Christiansen 
+ * @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @version    SVN: $Id$
+ * @link       http://code.google.com/p/janus-ssp/
+ * @since      Class available since Release 1.0.0
  */
-class sspmod_janus_User extends sspmod_janus_Database {
+class sspmod_janus_User extends sspmod_janus_Database
+{
+    /**
+     * Constant telling load() to load the user using the uid
+     */
+    const UID_LOAD = '__LOAD_WITH_UID__';
 
-	/**
-	 * Constant telling load() to load the user using the uid.
-	 */
-	const UID_LOAD = '__LOAD_WITH_UID__';
-	
-	/**
-	 * Constant telling load() to load the user using the email.
-	 */
-	const EMAIL_LOAD = '__LOAD_WITH_EMAIL__';
+    /**
+     * Constant telling load() to load the user using the email
+     */
+    const EMAIL_LOAD = '__LOAD_WITH_EMAIL__';
 
-	/**
-	 * Users uid
-	 * @var integer
-	 */
-	private $_uid;
-	
-	/**
-	 * User email
-	 * @var string
-	 */
-	private $_email;
-	
-	/**
-	 * User type
-	 * @var string
-	 */
-	private $_type;
+    /**
+     * User uid
+     * @var integer
+     */
+    private $_uid;
 
-	/**
-	 * User active status
-	 * @var string
-	 */
-	private $_active;
+    /**
+     * User email
+     * @var string
+     */
+    private $_email;
 
-	/**
-	 * User data.
-	 * @var array
-	 */
-	private $_data;
+    /**
+     * User type
+     * @var string
+     */
+    private $_type;
 
-	/**
-	 * Indicates whether the user data has been modified.
-	 * @var bool
-	 */
-	private $_modified = FALSE;
+    /**
+     * User active status
+     * @var string
+     */
+    private $_active;
 
-	/**
-	 * Class constructor.
-	 *
-	 * Class constructor that parses the configuration and initialize the user
-	 * object.
-	 *
-	 * @param array $config Configuration for the database
-	 */
-	public function __construct($config) {
-		// To start with only the store config is parsed til user
-		parent::__construct($config);
-	}
+    /**
+     * User data
+     * @var array
+     */
+    private $_data;
 
-	/**
-	 * Saves the user data to the database.
-	 *
-	 * Method for saving the user data to the database. If the user data has not
-	 * been modified the methos just returns TRUE. If an error occures and the
-	 * data is not saved the method returns FALSE.
-	 *
-	 * @return bool TRUE if data is saved end FALSE if data is not saved.
-	 * @todo Fix
-	 * 	- Clean up
-	 * 	- Remove exceptions, return TRUE/FALSE	
-	 */
-	public function save() {
-		// If the object is not modified, don't save it.
-		if(!$this->_modified) {
-			return TRUE;
-		}
-				
-		if(empty($this->_uid)) {
-			// Test if email address already exists
-			$st = $this->execute('SELECT count(*) AS `count` FROM '. self::$prefix .'__user WHERE `email` = ?;', array($this->_email));
-			if($st === FALSE) {
-				throw new Exception('JANUS:User:save - Error executing statement \'' . $statement . '\': ' . self::formatError($st->errorInfo()));
-			}
+    /**
+     * Indicates whether the user data has been modified or not
+     * @var bool
+     */
+    private $_modified = false;
 
-			$row = $st->fetchAll(PDO::FETCH_ASSOC);
-			if($row[0]['count'] > 0) {
-				throw new Exception('JANUS:User:save: Email already exists. Can not create new User.');
-			}
+    /**
+     * Create a new user 
+     *
+     * @param array &$config Databsee configuration
+     */
+    public function __construct(&$config)
+    {
+        parent::__construct($config);
+    }
 
-			// Create new User
-			$statement = 'INSERT INTO '. self::$prefix .'__user (`uid`, `type`, `email`, `active`, `update`, `created`, `ip`) VALUES (NULL, ?, ?, ?, ?, ?, ?)';
-			$st = $this->execute(
-				$statement,
-				array($this->_type, $this->_email, $this->_active,  date('c'), date('c'), $_SERVER['REMOTE_ADDR'])
-			);
+    /**
+     * Saves the user data to the database.
+     *
+     * Method for saving the user data to the database. If the user data has not
+     * been modified the methos just returns true. If an error occures and the
+     * data is not saved the method returns false.
+     *
+     * @return bool true if data is saved end false if data is not saved.
+     * @todo Fix
+     * 	- Clean up
+     * 	- Remove exceptions, return true/false	
+     */
+    public function save()
+    {
+        // If the user is not modified, just return
+        if (!$this->_modified) {
+            return true;
+        }
 
-			// Get new uid
-			$this->_uid = self::$db->lastInsertId();
-		} else {
-			$statement = 'UPDATE '. self::$prefix .'__user set `type` = ?, `email` = ?, `active` = ?, `update` = ?, `ip` = ?, `data` = ? WHERE `uid` = ?;';
-			$st = $this->execute(
-				$statement,
-				array($this->_type, $this->_email, $this->_active,  date('c'), $_SERVER['REMOTE_ADDR'], $this->_data, $this->_uid)
-			);
-		}
+        // uid is empty. This is a new user
+        if (empty($this->_uid)) {
+            // Test if email address already exists
+            $st = $this->execute(
+                'SELECT count(*) AS `count` 
+                FROM '. self::$prefix .'__user 
+                WHERE `email` = ?;', 
+                array($this->_email)
+            );
+            if ($st === false) {
+                throw new SimpleSAML_Error_Exception(
+                    'JANUS:User:save - Error executing statement : ' 
+                    .self::formatError($st->errorInfo())
+                );
+            }
 
-		if($st === FALSE) {
-			throw new Exception('JANUS:User:save - Error executing statement \'' . $statement . '\': ' . self::$db->errorInfo());
-		}
+            $row = $st->fetchAll(PDO::FETCH_ASSOC);
+            if ($row[0]['count'] > 0) {
+                throw new SimpleSAML_Error_Exception(
+                    'JANUS:User:save: Email already exists.
+                    Can not create new User.'
+                );
+            }
 
-		$this->_modified = FALSE;
+            // Create new User
+            $st = $this->execute(
+                'INSERT INTO '. self::$prefix .'__user 
+                (`uid`, `type`, `email`, `active`, `update`, `created`, `ip`) 
+                VALUES 
+                (null, ?, ?, ?, ?, ?, ?)',
+                array(
+                    $this->_type, 
+                    $this->_email, 
+                    $this->_active, 
+                    date('c'), 
+                    date('c'), 
+                    $_SERVER['REMOTE_ADDR'],
+                )
+            );
 
-		return TRUE;
-	}
+            // Get new uid
+            $this->_uid = self::$db->lastInsertId();
+        } else {
+            // Update existing user
+            $st = $this->execute(
+                'UPDATE '. self::$prefix .'__user set 
+                `type` = ?, 
+                `email` = ?, 
+                `active` = ?, 
+                `update` = ?, 
+                `ip` = ?, 
+                `data` = ? 
+                WHERE 
+                `uid` = ?;',
+                array(
+                    $this->_type, 
+                    $this->_email, 
+                    $this->_active, 
+                    date('c'), 
+                    $_SERVER['REMOTE_ADDR'], 
+                    $this->_data, 
+                    $this->_uid,
+                )
+            );
+        }
 
-	/**
-	 * Load user data from database.
-	 *
-	 * The methos loades the user data from the database, either by uid or by
-	 * email. Which depends on the flag parsed to the method. hest 
-	 *
-	 * @param const $flag Flag to indicate load method.
-	 * @return PDOStatement|bool The statement or FALSE if an error has occured.
-	 * @todo Fix 
-	 * 	- Skal kun returnere TRUE/FALSE (fjern exceptions)
-	 * 	- Proper validation of $st
-	 */
-	public function load($flag = self::UID_LOAD) {
+        if ($st === false) {
+            throw new SimpleSAML_Error_Exception(
+                'JANUS:User:save - Error executing statement : ' 
+                .self::$db->errorInfo()
+            );
+        }
 
-		if($flag === self::UID_LOAD) {
-			// Load user using uid
-			$st = $this->execute('SELECT * FROM '. self::$prefix .'__user WHERE `uid` = ?', array($this->_uid));
-		} else if($flag === self::EMAIL_LOAD) {	
-			// Load user using email
-			$st = $this->execute('SELECT * FROM '. self::$prefix .'__user WHERE `email` = ?', array($this->_email));
-		} else {
-			throw new Exception('JANUS:User:load: Invalid flag parsed - '. var_export($flag));
-		}
+        $this->_modified = false;
 
-		if($st === FALSE) {
-			throw new Exception('JANUS:User:save - Error executing statement \'' . $statement . '\': ' . self::$db->errorInfo());
-		}
+        return true;
+    }
 
-		$rs = $st->fetchAll(PDO::FETCH_ASSOC);
+    /**
+     * Load user data from database
+     *
+     * The methos loades the user data from the database, either by uid or by
+     * email. Which depends on the flag parsed to the method. uid is 
+     * preferrered. 
+     *
+     * @param const $flag Flag to indicate load method
+     *
+     * @return PDOStatement|bool The statement or false if an error has occured.
+     * @todo Fix 
+     * 	- Skal kun returnere true/false (fjern exceptions)
+     * 	- Proper validation of $st
+     */
+    public function load($flag = self::UID_LOAD)
+    {
+        if ($flag === self::UID_LOAD) {
+            // Load user using uid
+            $st = $this->execute(
+                'SELECT * 
+                FROM '. self::$prefix .'__user 
+                WHERE `uid` = ?',
+                array($this->_uid)
+            );
+        } else if ($flag === self::EMAIL_LOAD) {	
+            // Load user using email
+            $st = $this->execute(
+                'SELECT * 
+                FROM '. self::$prefix .'__user 
+                WHERE `email` = ?', 
+                array($this->_email)
+            );
+        } else {
+            throw new SimpleSAML_Error_Exception(
+                'JANUS:User:load: Invalid flag parsed - '
+                .var_export($flag)
+            );
+        }
 
-		if($row = $rs[0]) {
-			$this->_uid = $row['uid'];
-			$this->_email = $row['email'];
-			$this->_type = $row['type'];
-			$this->_active = $row['active'];
-			$this->_data = $row['data'];
+        if ($st === false) {
+            throw new SimpleSAML_Error_Exception(
+                'JANUS:User:save - Error executing statement : ' 
+                .self::$db->errorInfo()
+            );
+        }
 
-			$this->_modified = FALSE;
-		} else {
-			return FALSE;
-		}
+        $rs = $st->fetchAll(PDO::FETCH_ASSOC);
 
-		return $st;
-	}
-	
-	/**
-	 * Set user id.
-	 *
-	 * Method to set the user id. Method sets _modified to TRUE.
-	 *
-	 * @param int $uid User id
-	 */
-	public function setUid($uid) {
-		assert('ctype_digit($uid)');
+        if ($row = $rs[0]) {
+            $this->_uid = $row['uid'];
+            $this->_email = $row['email'];
+            $this->_type = $row['type'];
+            $this->_active = $row['active'];
+            $this->_data = $row['data'];
 
-		$this->_uid = $uid;
-	
-		$this->_modified = TRUE;
-	}
-	
-	/**
-	 * Det user email.
-	 *
-	 * Method for setting the user email. The method does not validate the
-	 * correctness of the email, only that it is a string and that is is not
-	 * longer that 320 chars. Method sets _modified to TRUE.
-	 *
-	 * @param string $email User email.
-	 * @todo Validate email. 
-	 */ 
-	public function setEmail($email) {
-		assert('is_string($email)');
-		assert('strlen($email) <= 320');
-		
-		$this->_email = $email;
+            $this->_modified = false;
+        } else {
+            return false;
+        }
+        return $st;
+    }
 
-		$this->_modified = TRUE;
-	}
+    /**
+     * Set user id
+     *
+     * Method to set the user id. Method sets _modified to true.
+     *
+     * @param int $uid User id
+     *
+     * @return void
+     */
+    public function setUid($uid)
+    {
+        assert('ctype_digit($uid)');
 
-	/**
-	 * Set user type.
-	 *
-	 * Method for setting the user type. Method sets _modified to TRUE.
-	 *
-	 * @param string $type User type.
-	 * @todo Test that type is valid according to the config.
-	 */
-	public function setType($type) {
-		assert('is_string($type)');
+        $this->_uid = $uid;
 
-		$this->_type = $type;
+        $this->_modified = true;
+    }
 
-		$this->_modified = TRUE;
-	}
+    /**
+     * Set user email
+     *
+     * Method for setting the user email. The method does not validate the
+     * correctness of the email, only that it is a string and that is is not
+     * longer that 320 chars. Method sets _modified to true.
+     *
+     * @param string $email User email
+     *
+     * @return void
+     */ 
+    public function setEmail($email) 
+    {
+        assert('is_string($email)');
+        assert('strlen($email) <= 320');
 
-	public function setActive($active) {
-		assert('is_string($active)');
+        $this->_email = $email;
 
-		$this->_active = $active;
+        $this->_modified = true;
+    }
 
-		$this->_modified = TRUE;
-	}
+    /**
+     * Set user type
+     *
+     * Method for setting the user type. Method sets _modified to true.
+     *
+     * @param string $type User type
+     *
+     * @return void
+     * @todo Test that type is valid according to the config.
+     */
+    public function setType($type)
+    {
+        assert('is_string($type)');
 
-	public function getActive() {
-		return $this->_active;
-	}
+        $this->_type = $type;
 
-	/**
-	 * Get user id.
-	 *
-	 * Method for getting the user id.
-	 *
-	 * @return int The user id.
-	 */
-	public function getUid() {
-		return $this->_uid;
-	}
+        $this->_modified = true;
+    }
 
-	/**
-	 * Get user email.
-	 *
-	 * Method for getting the user email.
-	 *
-	 * @return string The user email.
-	 */
-	public function getEmail() {
-		return $this->_email;
-	}
+    /**
+     * Set user active
+     *
+     * @param string $active 'yes'/'no'
+     *
+     * @return void
+     * @since Method available since Release 1.0.0
+     * @todo Change active type to bool
+     */
+    public function setActive($active)
+    {
+        assert('is_string($active)');
 
-	/**
-	 * Get user type.
-	 *
-	 * Method for getting the user type.
-	 *
-	 * @return string The user type.
-	 */
-	public function getType() {
-		return $this->_type;	
-	}
+        $this->_active = $active;
 
-	public function getData() {
-		return $this->_data;
-	}
+        $this->_modified = true;
+    }
 
-	/**
-	 * Get modified information.
-	 *
-	 * Method for getting the status of the _modified variable.
-	 *
-	 * @return bool TRUE in user data is modified.
-	 */
-	public function isModified() {
-		return $this->_modified;
-	}
-	
-	public function setData($data) {
-		assert('is_string($data)');
+    /**
+     * Get user active
+     *
+     * @return string 'yes'/'no'
+     * @since Method available since Release 1.0.0
+     */
+    public function getActive()
+    {
+        return $this->_active;
+    }
 
-		$this->_data = $data;
+    /**
+     * Get user id
+     *
+     * Method for getting the user id.
+     *
+     * @return int The user id.
+     */
+    public function getUid()
+    {
+        return $this->_uid;
+    }
 
-		$this->_modified = TRUE;
-	}
+    /**
+     * Get user email
+     *
+     * Method for getting the user email.
+     *
+     * @return string The user email.
+     */
+    public function getEmail() 
+    {
+        return $this->_email;
+    }
 
+    /**
+     * Get user type
+     *
+     * Method for getting the user type.
+     *
+     * @return string The user type.
+     */
+    public function getType() 
+    {
+        return $this->_type;	
+    }
+
+    /**
+     * Get user data
+     *
+     * @return string The user data
+     * @since Method available since Release 1.0.0
+     */
+    public function getData() 
+    {
+        return $this->_data;
+    }
+
+    /**
+     * Get modified information.
+     *
+     * Method for getting the status of the _modified variable.
+     *
+     * @return bool true in user data is modified.
+     */
+    public function isModified() 
+    {
+        return $this->_modified;
+    }
+
+    /**
+     * Set user data
+     *
+     * The way additional data about a user is stored will be changed in the 
+     * future.
+     *
+     * @param string $data The user data
+     *
+     * @return void
+     * @since Method available since Release 1.0.0
+     */
+    public function setData($data) 
+    {
+        assert('is_string($data)');
+
+        $this->_data = $data;
+
+        $this->_modified = true;
+    }
 }
 ?>
