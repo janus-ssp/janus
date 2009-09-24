974886d3f5ad781515fb970eb140ae77dbc0fcc8
Jacob Christiansen <jacob@colourbox.com>
2009-09-24 11:44:25 +0000

    Fixes issue 52 Added support for spanish Patch provided by pitbulk

--- dictionaries/janus.php
+++ dictionaries/janus.php
@@ -1,365 +1,437 @@
 <?php
+/*
+ * @author Jacob Christiansen, <jach@wayf.dk>
+ * @author pitbulk
+ */
 // Dictionary for JANUS module
 $lang = array(
 	
 	// new user		  
 	'text_new_user_created' => array(
-		'da' => 'En brugerkonto er nu oprettet for %USERMAIL%.<br /> 
-		For at indtaste flere oplysninger om dig selv, følg linket.',
-		'en' => 'A user account has been created for %USERMAIL%.<br />
-		To enter more information about yourself, folow the link.',		
+		'da' => 'En brugerkonto er nu oprettet for %USERMAIL%.<br /> For at indtaste flere oplysninger om dig selv, følg linket.',
+		'en' => 'A user account has been created for %USERMAIL%.<br /> To enter more information about yourself, follow the link.',
+		'es' => 'Una cuenta de usuario ha sido creada para %USERMAIL%.<br /> Para introducir más información acerca de ti, sigue el enlace.',
 	),
 	
 	'header_new_user' => array(
 		'da' => 'Opret ny brugerkonto',
-		'en' => 'Create new user account',		
+		'en' => 'Create new user account',
+		'es' => 'Crear una nueva cuenta de usuario',
 	),
 	
 	'text_create_new_user' => array(
 		'da' => 'Du er ved at oprette en konto i JANUS med brugernavnet %USERMAIL%.<br />Tryk på enter for at oprette en konto.',
-		'en' => 'You are about to create an account I JANUS with the username %USERMAIL%.<br />Push enter to create account',		
+		'en' => 'You are about to create an account I JANUS with the username %USERMAIL%.<br />Push enter to create account',
+		'es' => 'Vas a crear una cuenta de usuario en JANUS con el nombre de usuario %USERMAIL%.<br />Pulsa enter para crear la cuenta',
 	),
 
 	// Misc
 	'text_select_type' => array(
 		'da' => 'Vælg type',
-		'en' => 'Please select type',		
+		'en' => 'Please select type',
+		'es' => 'Por favor selecciona un tipo',
 	),
 	
 	'text_submit_button' => array(
 		'da' => 'Opret',
-		'en' => 'Create',		
+		'en' => 'Create',
+		'es' => 'Crear',
 	),
 	
 	'text_dashboard' => array(
 		'da' => 'Kontrolpanel',
-		'en' => 'Dashboard',		
+		'en' => 'Dashboard',
+		'es' => 'Panel',
 	),
 
 	'text_entity_created' => array(
 		'da' => 'Forbindelse oprettet',
-		'en' => 'Connection created',		
+		'en' => 'Connection created',
+		'es' => 'Conexión creada',
 	),
 
 	'text_saml20-idp' => array(
 		'da' => 'Institutioner (IdP)',
-		'en' => 'Identity Providers (IdP)',		
+		'en' => 'Identity Providers (IdP)',
+		'es' => 'Proveedores de Identidad (IdP)',
 	),
 
 	'text_saml20-sp' => array(
 		'da' => 'Tjenester (SP)',
-		'en' => 'Service Providers (SP)',		
+		'en' => 'Service Providers (SP)',
+		'es' => 'Proveedores de Servicio',
 	),
 
 	'text_shib13-sp' => array(
 		'da' => 'Shib Tjenester (SP)',
-		'en' => 'Shib Service Providers (SP)',		
+		'en' => 'Shib Service Providers (SP)',
+		'es' => 'Proveedores de Servicio Shib (SP)',
 	),
 
 	'text_shib13-idp' => array(
-		'da' => 'Shib Tjenester (SP)',
-		'en' => 'Shib Service Providers (SP)',		
+		'da' => 'Shib Institutioner (IdP)',
+		'en' => 'Shib Identity Providers (IdP)',
+		'es' => 'Proveedores de Identidad Shib (IdP)',
 	),
 
 	// Dashboard Tabs
 	'tab_user_data_header' => array(
 		'da' => 'Bruger',
-		'en' => 'User',		
+		'en' => 'User',
+		'es' => 'Usuario',
 	),
 
 	'tab_user_data_subheader' => array(
 		'da' => 'Konto oplyninger',
-		'en' => 'Account information',		
+		'en' => 'Account information',
+		'es' => 'Información de la cuenta',
 	),
 
 	'tab_user_data_username' => array(
 		'da' => 'Brugernavn',
-		'en' => 'User name',		
+		'en' => 'User name',
+		'es' => 'Nombre de usuario',
 	),
 
 	'tab_user_data_otherinfo' => array(
 		'da' => 'Andre oplysninger',
-		'en' => 'Other informations',		
+		'en' => 'Other informations',
+		'es' => 'Otra información',
 	),
 	
 	'tab_entities_header' => array(
 		'da' => 'Forbindelser',
-		'en' => 'Connections',		
+		'en' => 'Connections',
+		'es' => 'Conexiones',
 	),
 
-	'tab_message_header' => array(
-		'da' => 'Inbakke',
-		'en' => 'Inbox',		
-	),
+    'tab_message_header' => array(
+        'da' => 'Indbakke',
+        'en' => 'Inbox',        
+    ),
 
 	'tab_entities_new_entity_subheader' => array(
 		'da' => 'Ny forbindelse',
-		'en' => 'New Connection',		
+		'en' => 'New Connection',
+		'es' => 'Nueva conexión',
 	),
 
 	'tab_entities_new_entity_text' => array(
 		'da' => 'Indtast nyt forbindelsesID',
-		'en' => 'Enter new connection ID',		
+		'en' => 'Enter new connection ID',
+		'es' => 'Introducir nueva ID de conexión',
 	),
 	
 	'tab_entities_entities_subheader' => array(
 		'da' => 'Eksisterende forbindelse',
-		'en' => 'Existing Connection',		
+		'en' => 'Existing Connection',
+		'es' => 'Conexiones existentes',
 	),
 
 	'tab_admin_header' => array(
 		'da' => 'Administrator',
 		'en' => 'Admin',
+		'es' => 'Administrador',
 	),
 
 	'tab_admin_tab_users_header' => array(
 		'da' => 'Brugere',
 		'en' => 'Users',
+		'es' => 'Usuarios',
 	),
 	
 	'tab_admin_tab_entities_header' => array(
 		'da' => 'Forbindelser',
 		'en' => 'Connections',
+		'es' => 'Conexiones',
 	),
 	
 	// Errors
 	'error_header' => array(
 		'da' => 'Fejl',
-		'en' => 'Error',		
+		'en' => 'Error',
+		'es' => 'Error',
 	),
 	
 	'error_entity_exists' => array(
 		'da' => 'Forbindelses ID findes i forvejen',
-		'en' => 'Connection ID already exists'
+		'en' => 'Connection ID already exists',
+		'es' => 'La ID de conexion ya existe',
 	),
 
     'error_entity_not_url' => array(
         'da' => 'Forbindelses ID skal være en gyldig URL jf. RFC1738.<br />Kun alfanumeriske tegn og $&\'-_.,;=+!*()~/% kan bruges i forbindelses ID\'er.',
-        'en' => 'The connection ID should be a valid URL complying to the RFC1738.</br />Only alphanumeric characters and $&\'-_.,;=+!*()~/% may be used in entity ids.',        
+        'en' => 'The connection ID should be a valid URL complying to the RFC1738.</br />Only alphanumeric characters and $&\'-_.,;=+!*()~/% may be used in entity ids.',
+	'es' => 'La ID de conexión debería ser una URL válida que cumpla en RFC1738.</br>Solo caracteres alfanuméricos y $&\'-_.,;=+!*()~/% deben ser usados en los identificadores de indentidad.',
     ),
 
     'error_no_type' => array(
         'da' => 'Angiv forbindelsestype',
-        'en' => 'Please select connection type',        
+        'en' => 'Please select connection type',
+	'es' => 'Por favor seleccione un tipo de conexión',
     ),
 	
     //Edit entity tabs
 	'edit_entity_header' => array(
 		'da' => 'Rediger forbindelse',
-		'en' => 'Edit connection',		
+		'en' => 'Edit connection',
+		'es' => 'Editar conexión',
 	),
 	
 	'tab_edit_entity_connection' => array(
 		'da' => 'Forbindelse',
-		'en' => 'Connection',		
+		'en' => 'Connection',
+		'es' => 'Conexión',
 	),
 
 	'tab_edit_entity_connection_revision' => array(
 		'da' => 'Revision',
-		'en' => 'Revision',		
+		'en' => 'Revision',
+		'es' => 'Revisión',
 	),
 	
 	'tab_edit_entity_revision_note' => array(
 		'da' => 'Revisionsnote',
-		'en' => 'Revision note',		
+		'en' => 'Revision note',
+		'es' => 'Notas de la revisión',
 	),
 	
 	'tab_edit_entity_history' => array(
 		'da' => 'Historik',
-		'en' => 'History',		
+		'en' => 'History',
+		'es' => 'Histórico',
 	),
 	
 	'tab_edit_entity_show_hide' => array(
 		'da' => 'Vis/Gem',
-		'en' => 'Show/Hide',		
+		'en' => 'Show/Hide',
+		'es' => 'Mostrar/Ocultar',
 	),
 	
 	'tab_edit_entity_export' => array(
 		'da' => 'Eksporter',
-		'en' => 'Export',		
+		'en' => 'Export',
+		'es' => 'Exportar',
 	),
 	
 	'tab_edit_entity_export_metadata' => array(
 		'da' => 'Eksporter metadata',
-		'en' => 'Export metadata',		
+		'en' => 'Export metadata',
+		'es' => 'Exportar medatados',
 	),
 	
     'tab_edit_entity_save' => array(
         'da' => 'Gem',
-        'en' => 'Save',		
+        'en' => 'Save',
+	'es' => 'Guardar',
     ),
 	
     'tab_edit_entity_select' => array(
 		'da' => 'Vælg',
-		'en' => 'Select',		
+		'en' => 'Select',
+		'es' => 'Seleccionar',
 	),
 	
 	'tab_edit_entity_import_xml' => array(
 		'da' => 'Importer XML',
-		'en' => 'Import XML',		
+		'en' => 'Import XML',
+		'es' => 'Importar XML',
 	),
 	
     'tab_edit_entity_entry' => array(
         'da' => 'Felt',
-        'en' => 'Entry',		
+        'en' => 'Entry',
+	'es' => 'Entrada',
     ),
 
 	'tab_edit_entity_value' => array(
 		'da' => 'Værdi',
-		'en' => 'Value',		
+		'en' => 'Value',
+		'es' => 'Valor',
 	),
 	
     'tab_edit_entity_parent_revision' => array(
         'da' => 'Forrig revision',
-        'en' => 'Parent revision',		
+        'en' => 'Parent revision',
+	'es' => 'Revisión padre',
     ),
     
     'tab_edit_entity_state' => array(
         'da' => 'Tilstand',
-        'en' => 'State',		
+        'en' => 'State',
+	'es' => 'Estado',
     ),
 
 
 	'tab_edit_entity_connection_entityid' => array(
 		'da' => 'Forbindelses ID',
-		'en' => 'Connection ID',		
+		'en' => 'Connection ID',
+		'es' => 'ID de conexión',
 	),
 	
 	'tab_remote_entity_saml20-sp' => array(
 		'da' => 'Institution (IdP)',
-		'en' => 'Indentity Provider (IdP)',		
+		'en' => 'Indentity Provider (IdP)',
+		'es' => 'Proveedores de Identidad (IdP)',
 	),
 	
 	'tab_remote_entity_saml20-idp' => array(
 		'da' => 'Tjenester (SP)',
-		'en' => 'Service Provider (SP)',		
+		'en' => 'Service Provider (SP)',
+		'es' => 'Proveedores de Servicio (SP)',
 	),
 
 	'tab_remote_entity_help_saml20-idp' => array(
 		'da' => 'Marker de tjenester som du ikke ønsker dine brugere skal have adgang til.',
-		'en' => 'Mark services that your users are not allowed to access.',		
+		'en' => 'Mark services that your users are not allowed to access.',
+		'es' => 'Marca los servicios a los que a tus usuarios no les está permitido acceder',
+
 	),
 
 	'tab_remote_entity_help_saml20-sp' => array(
 		'da' => 'Marker de institutioner som du ikke ønsker brugere fra.',
-		'en' => 'Mark those identity providers which are not allowed to acces your service.',		
+		'en' => 'Mark those identity providers which are not allowed to acces your service.',
+		'es' => 'Marca aquellos Proveedores de identidad a los que no les está permitido el acceso a tu servicio.', 
 	),
 
 	'tab_remote_entity_allowall' => array(
 		'da' => 'Tillad alle',
-		'en' => 'Allow all',		
+		'en' => 'Allow all',
+		'es' => 'Permitir todos',
 	),
 
 	'tab_metadata' => array(
 		'da' => 'Metadata',
-		'en' => 'Metadata',		
+		'en' => 'Metadata',
+		'es' => 'Metadatos',
 	),
 
 	'tab_import_metadata' => array(
 		'da' => 'Importer metadata',
-		'en' => 'Import metadata,'		
+		'en' => 'Import metadata',
+		'es' => 'Importar metadatos',
 	),
 	
 	'text_entities_help' => array(
 		'da' => 'Klik på en tjeneste eller institution for at administrere forbindelser.',
-		'en' => 'Click on a service provider or identity provider to administer connections.',		
+		'en' => 'Click on a service provider or identity provider to administer connections.',
+		'es' => 'Haz click sobre el proveedor de servicio o el identificador de identidad para administrar las conexiones.',
 	),
 	
 	'status_metadata_parsed_ok' => array(
 		'da' => 'Metadata importeret korrekt',
-		'en' => 'Metadata imported',		
+		'en' => 'Metadata imported',
+		'es' => 'Metadatos importados',
 	),
 	
 	'error_entityid_no_match' => array(
 		'da' => 'Entity id i metadata er ikke korrekt',
-		'en' => 'Entity id in metadata do not match',		
+		'en' => 'Entity id in metadata do not match',
+		'es' => 'El identificador de entidad no concuerda con los metadatos',
 	),
 	
 	'error_metadata_not_parsed' => array(
 		'da' => 'Fejl ved import af metadata',
-		'en' => 'Error when parsing imported metadata',		
+		'en' => 'Error when parsing imported metadata',
+		'es' => 'Error mientras se parseaban los metadatos importados',
 	),
 	
 	// Export
 	'export_text' => array(
 		'da' => 'Her under kan du se dit metadata',
-		'en' => 'Here you can se your metadata',		
+		'en' => 'Here you can see your metadata',
+		'es' => 'Aquí puedes ver tus metadatos',
 	),
 	
 	'export_intro' => array(
 		'da' => 'Her under kan du se dit metadata',
-		'en' => 'Here you can se your metadata',		
+		'en' => 'Here you can see your metadata',
+		'es' => 'Aquí puedes ver tus metadatos',
 	),
 
     // Admin tab
 	'admin_permission' => array(
 		'da' => 'Tilladelser',
-		'en' => 'Permissions',		
+		'en' => 'Permissions',
+		'es' => 'Permisos',
 	),
 	
     'admin_users' => array(
 		'da' => 'Brugere',
-		'en' => 'Users',		
+		'en' => 'Users',
+		'es' => 'Usuarios',
 	),
 
     'admin_add' => array(
 		'da' => 'Tilføj',
-		'en' => 'Add',		
+		'en' => 'Add',
+		'es' => 'Añadir',
 	),
 
     'admin_remove' => array(
 		'da' => 'Fjern',
-		'en' => 'Remove',		
+		'en' => 'Remove',
+		'es' => 'Borrar',
 	),
 
     'admin_action' => array(
 		'da' => 'Handling',
-		'en' => 'Action',		
+		'en' => 'Action',
+		'es' => 'Acción',
 	),
 
     'admin_delete' => array(
 		'da' => 'Slet',
-		'en' => 'Delete',		
+		'en' => 'Delete',
+		'es' => 'Eliminar',
 	),
 
     'admin_type' => array(
 		'da' => 'Type',
-		'en' => 'Type',		
+		'en' => 'Type',
+		'es' => 'Tipo',
 	),
 
     'admin_email' => array(
         'da' => 'E-mail',
-        'en' => 'E-mail',		
+        'en' => 'E-mail',
+	'es' => 'E-mail',
     ),
 
 	'admin_select_remove_user' => array(
 		'da' => 'Vælg bruger som skal fjernes',
-		'en' => 'Select user to remove',		
+		'en' => 'Select user to remove',
+		'es' => 'Selecciona usuario a eliminar',
 	),
 	
 	'admin_select_add_user' => array(
 		'da' => 'Vælg bruger som skal tilføjes',
-		'en' => 'Select user to add',		
+		'en' => 'Select user to add',
+		'es' => 'Selecciona usuario a añadir',
 	),
 	
 	'accessblocker_header' => array(
 		'da' => 'Adgang blokret',
-		'en' => 'Access blocked',		
+		'en' => 'Access blocked',
+		'es' => 'Acceso bloqueado',
 	),
 
     'error_no_access' => array(
         'da' => 'Du har ikke adgang til denne ressource',
-        'en' => 'You do not have access to this resource'        
+        'en' => 'You do not have access to this resource',
+	'es' => 'No tienes acceso para acceder a este recurso',
     ),
     
     'text_help' => array(
         'da' => 'Hjælp',
-        'en' => 'Help',        
+        'en' => 'Help',
+	'es' => 'Ayuda',
     ),
     /*
 	'' => array(
 		'da' => '',
-		'en' => '',		
+		'en' => '',
+		'es' => '',
 	),
 	*/
 );
--- dictionaries/mailtoken.php
+++ dictionaries/mailtoken.php
@@ -5,12 +5,14 @@ $lang = array(
 	// Error messages for MailToken login
 	'error_mail_not_valid' => array(
 		'da' => 'Email addressen du indtastede er ikke korrekt. Prøv igen.',
-		'en' => 'The email address you just entered is not valid. Please try again.',		
+		'en' => 'The email address you just entered is not valid. Please try again.',
+		'es' => 'El email que ha introducido es inválido. Por favor intentelo de nuevo.',
 	),
 	
 	'error_mail_not_send' => array(
 		'da' => 'Der er sket en fejl. Der er ikke sendt en mail. Prøv igen.',
-		'en' => 'An error has occured. Please try again.',		
+		'en' => 'An error has occured. Please try again.',
+		'es' => 'Ha ocurrido un error. Por favor intentelo de nuevo.',
 	),
 	
 	// Status messages for MailToken login
@@ -21,61 +23,74 @@ $lang = array(
 		'en' => 'JANUS has registered the email address.<br />
 		An email with a link has been sent to %USERMAIL%.<br />
 		To log into the JANUS, click the link in the email.',
+		'es' => 'JANUS ha registrado su dirección de correo.<br />
+		Le ha sido enviado un correo con un enlace a %USERMAIL%.<br />
+		Para acceder a JANUS, haga click en el enlace del correo.',
 	),
 
 	'send_mail_new_token_by_old' => array(
 		'da' => 'Du kan kun logge ind én gang med den samme link.<br />Vi har sendt dig en ny loginlink. Du modtager en mail med en ny login link.',
-		'en' => 'It is not possible to reuse your login link.<br />You will receive a mail containing a new login link.',		
+		'en' => 'It is not possible to reuse your login link.<br />You will receive a mail containing a new login link.',
+		'es' => 'No es posible reutilizar el enlace de acceso.<br />Recibirás un correo que contenga un nuevo enlace de acceso.',
 	),
 
 	// Text
 	'help_header' => array(
 		'da' => 'Hjælp',
 		'en' => 'Help',
+		'es' => 'Ayuda',
 	),
 
 	'help_text' => array(
 		'da' => 'I tilfælde af problemer med JANUS, kontakt %ADMINNAME%, <a href="mailto:%ADMINEMAIL%">%ADMINEMAIL%</a>',
-		'en' => 'In case of problems contact %ADMINNAME%, <a href="mailto:%ADMINEMAIL%">%ADMINEMAIL%</a>',		
+		'en' => 'In case of problems contact %ADMINNAME%, <a href="mailto:%ADMINEMAIL%">%ADMINEMAIL%</a>',
+		'es' => 'Si tiene problemas contacte con %ADMINNAME%, <a href="mailto:%ADMINEMAIL%">%ADMINEMAIL%</a>',
 	),
 
 	// Login text	
 	'text_login_header' => array(
 		'da' => 'Login',
-		'en' => 'Login',		
+		'en' => 'Login',
+		'es' => 'Acceso',
 	),
 	
 	'text_login_help' => array(
 		'da' => 'For at logge ind skal du svare på en kontrol-email.<br /> Indtast din adresse, og klik på linket i den email, du modtager.',
-		'en' => 'To login you need to recive an email.<br/>Enter your enail address og click the link in the recived email.',		
+		'en' => 'To login you need to recive an email.<br/>Enter your email address or click the link in the recived email.',
+		'es' => 'Para acceder necesitas recibir un correo..<br/> Introduce una dirección de correo o clickea el enlace del correo que recibiste.',
 	),
 
 	// New user text
 	'text_create_account_header' => array(
 		'da' => 'Kontooprettelse',
-		'en' => 'New accoount',		
+		'en' => 'New accoount',
+		'es' => 'Nueva cuenta',
 	),
 
 	'text_create_account_help' => array(
 		'da' => 'For at blive oprettet som bruger skal du svare på en kontrol-email.<br />Indtast din adresse, og klik på linket i den email, du modtager.',
-		'en' => 'To create a new account you ned to recive an email.<br />Enter your enail address og click the link in the recived email.',		
+		'en' => 'To create a new account you need to recive an email.<br />Enter your email address or click the link in the recived email.',
+		'es' => 'Para crear una nueva cuenta necesitas recibir un correo.<br />Introduce tu dirección de correo o clickea el enlace del correo que recibiste.',
 	),
 
 	// Misc text
 	'text_send_button' => array(
 		'da' => 'Send',
-		'en' => 'Send',		
+		'en' => 'Send',
+		'es' => 'Enviar',
 	),
 
 	'text_success_header' => array(
 		'da' => 'Succes',
-		'en' => 'Success',		
+		'en' => 'Success',
+		'es' => 'Éxito',
 	),
 
 	/*
 	'' => array(
 		'da' => '',
-		'en' => '',		
+		'en' => '',
+		'es' => '',
 	),
 	*/
 );
