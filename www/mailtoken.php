dcce1111ff758521f88b9a377337dba15f921eee
Jacob Christiansen <jacob@colourbox.com>
2009-11-12 09:47:24 +0000

    Minor update to mailtoken

--- www/mailtoken.php
+++ www/mailtoken.php
@@ -11,24 +11,26 @@ $authStateId = $_REQUEST['AuthState'];
 /* Retrieve the authentication state. */
 $state = SimpleSAML_Auth_State::loadState($authStateId, sspmod_janus_Auth_Source_MailToken::STAGEID);
 
+$mail = null;
+$token = null;
+
+if(array_key_exists('mail', $_REQUEST)) {
+    $mail = $_REQUEST['mail'];
+}
+
 if(array_key_exists('SPMetadata', $state)) {
-    if(array_key_exists('token', $state['SPMetadata']) && array_key_exists('mail', $state['SPMetadata'])) {
+    if(array_key_exists('token', $state['SPMetadata'])) {
         $token = $state['SPMetadata']['token'];
+    }
+    if (array_key_exists('mail', $state['SPMetadata'])) {
         $mail = $state['SPMetadata']['mail'];
-        if(array_key_exists('mail', $_REQUEST)) {
-            $mail = $_REQUEST['mail'];
-        }
-        $error = sspmod_janus_Auth_Source_MailToken::handleLogin($authStateId, $mail, $token);
     }
 }
 
-//echo "<pre>";
-//var_dump($state);
-//echo "</pre>";
+$error = sspmod_janus_Auth_Source_MailToken::handleLogin($authStateId, $mail, $token);
 
 $adminname = $janus_config->getValue('admin.name', 'No admin configured');
 $adminemail = $janus_config->getValue('admin.email', 'No admin email configured');
-
 
 $t = new SimpleSAML_XHTML_Template($config, 'janus:mailtoken.php', 'janus:mailtoken');
 
