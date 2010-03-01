5684a48b369b9be35ed789d1046d65b7e538640d
Jacob Christiansen <jacob@colourbox.com>
2010-03-01 09:46:20 +0000

    Fixes issue 102 Updated SQL file

--- docs/janus.sql
+++ docs/janus.sql
@@ -1,27 +1,51 @@
--- MySQL dump 10.11
+-- phpMyAdmin SQL Dump
+-- version 3.2.4
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: jach_db
--- ------------------------------------------------------
--- Server version	5.0.32-Debian_7etch8-log
+-- Host: localhost
+-- Generation Time: Mar 01, 2010 at 10:43 AM
+-- Server version: 5.0.32
+-- PHP Version: 5.2.0-8+etch13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `janus_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `janus__arp`
+--
+
+DROP TABLE IF EXISTS `janus__arp`;
+CREATE TABLE IF NOT EXISTS `janus__arp` (
+  `aid` int(11) NOT NULL auto_increment,
+  `name` text,
+  `description` text,
+  `attributes` text,
+  `created` char(25) NOT NULL,
+  `updated` char(25) NOT NULL,
+  `ip` char(15) NOT NULL,
+  PRIMARY KEY  (`aid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=56 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `janus__attribute`
 --
 
 DROP TABLE IF EXISTS `janus__attribute`;
-CREATE TABLE `janus__attribute` (
-  `entityid` text NOT NULL,
+CREATE TABLE IF NOT EXISTS `janus__attribute` (
+  `eid` int(11) NOT NULL,
   `revisionid` int(11) NOT NULL,
   `key` text NOT NULL,
   `value` text NOT NULL,
@@ -29,25 +53,44 @@ CREATE TABLE `janus__attribute` (
   `ip` char(15) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `janus__blockedEntity`
 --
 
 DROP TABLE IF EXISTS `janus__blockedEntity`;
-CREATE TABLE `janus__blockedEntity` (
-  `entityid` text NOT NULL,
+CREATE TABLE IF NOT EXISTS `janus__blockedEntity` (
+  `eid` int(11) NOT NULL,
   `revisionid` int(11) NOT NULL,
   `remoteentityid` text NOT NULL,
   `created` char(25) NOT NULL,
   `ip` char(15) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `janus__disableConsent`
+--
+
+DROP TABLE IF EXISTS `janus__disableConsent`;
+CREATE TABLE IF NOT EXISTS `janus__disableConsent` (
+  `eid` int(11) NOT NULL,
+  `revisionid` int(11) NOT NULL,
+  `remoteentityid` text NOT NULL,
+  `created` char(25) NOT NULL,
+  `ip` char(15) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `janus__entity`
 --
 
 DROP TABLE IF EXISTS `janus__entity`;
-CREATE TABLE `janus__entity` (
+CREATE TABLE IF NOT EXISTS `janus__entity` (
   `eid` int(11) NOT NULL,
   `entityid` text NOT NULL,
   `revisionid` int(11) default NULL,
@@ -56,106 +99,36 @@ CREATE TABLE `janus__entity` (
   `expiration` char(25) default NULL,
   `metadataurl` text,
   `allowedall` char(3) NOT NULL default 'yes',
+  `arp` int(11) default NULL,
   `created` char(25) default NULL,
   `ip` char(15) default NULL,
   `parent` int(11) default NULL,
-  `revisionnote` text
+  `revisionnote` text,
+  UNIQUE KEY `eid` (`eid`,`revisionid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `janus__hasEntity`
 --
 
 DROP TABLE IF EXISTS `janus__hasEntity`;
-CREATE TABLE `janus__hasEntity` (
+CREATE TABLE IF NOT EXISTS `janus__hasEntity` (
   `uid` int(11) NOT NULL,
   `eid` int(11) default NULL,
   `created` char(25) default NULL,
   `ip` char(15) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Table structure for table `janus__metadata`
---
-
-DROP TABLE IF EXISTS `janus__metadata`;
-CREATE TABLE `janus__metadata` (
-  `eid` int(11) NOT NULL,
-  `revisionid` int(11) NOT NULL,
-  `key` text NOT NULL,
-  `value` text NOT NULL,
-  `created` char(25) NOT NULL,
-  `ip` char(15) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Table structure for table `janus__tokens`
---
-
-DROP TABLE IF EXISTS `janus__tokens`;
-CREATE TABLE `janus__tokens` (
-  `id` int(11) NOT NULL auto_increment,
-  `mail` varchar(320) NOT NULL,
-  `token` varchar(255) NOT NULL,
-  `notvalidafter` varchar(255) NOT NULL,
-  `usedat` varchar(255) default NULL,
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `token` (`token`)
-) ENGINE=MyISAM AUTO_INCREMENT=170 DEFAULT CHARSET=latin1;
-
---
--- Table structure for table `janus__user`
---
-
-DROP TABLE IF EXISTS `janus__user`;
-CREATE TABLE `janus__user` (
-  `uid` int(11) NOT NULL auto_increment,
-  `userid` text,
-  `type` text,
-  `email` varchar(320) default NULL,
-  `active` char(3) default 'yes',
-  `update` char(25) default NULL,
-  `created` char(25) default NULL,
-  `ip` char(15) default NULL,
-  `data` text,
-  PRIMARY KEY  (`uid`)
-) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
-
---
--- Table structure for table `janus__userData`
---
-
-DROP TABLE IF EXISTS `janus__userData`;
-CREATE TABLE `janus__userData` (
-  `uid` int(11) NOT NULL,
-  `key` varchar(255) NOT NULL,
-  `value` varchar(255) NOT NULL,
-  `update` char(25) NOT NULL,
-  `created` char(25) NOT NULL,
-  `ip` char(15) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Table structure for table `janus__subscription`
---
-
-DROP TABLE IF EXISTS `janus__subscription`;
-CREATE TABLE `janus__subscription` (
-  `sid` int(11) NOT NULL auto_increment,
-  `uid` int(11) NOT NULL,
-  `subscription` text NOT NULL,
-  `type` text,
-  `created` char(25) default NULL,
-  `ip` char(15) default NULL,
-  PRIMARY KEY  (`sid`)
-) ENGINE=MyISAM AUTO_INCREMENT=87 DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `janus__message`
 --
 
 DROP TABLE IF EXISTS `janus__message`;
-CREATE TABLE `janus__message` (
+CREATE TABLE IF NOT EXISTS `janus__message` (
   `mid` int(11) NOT NULL auto_increment,
   `uid` int(11) NOT NULL,
   `subject` text NOT NULL,
@@ -166,28 +139,91 @@ CREATE TABLE `janus__message` (
   `created` char(25) NOT NULL,
   `ip` char(15) default NULL,
   PRIMARY KEY  (`mid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2255 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=440 ;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `janus__disableConsent`
+-- Table structure for table `janus__metadata`
 --
 
-DROP TABLE IF EXISTS `janus__disableConsent`;
-CREATE TABLE `janus__disableConsent` (
+DROP TABLE IF EXISTS `janus__metadata`;
+CREATE TABLE IF NOT EXISTS `janus__metadata` (
   `eid` int(11) NOT NULL,
   `revisionid` int(11) NOT NULL,
-  `remoteentityid` text NOT NULL,
+  `key` text NOT NULL,
+  `value` text NOT NULL,
   `created` char(25) NOT NULL,
   `ip` char(15) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- --------------------------------------------------------
 
--- Dump completed on 2009-11-12  8:48:48
+--
+-- Table structure for table `janus__subscription`
+--
+
+DROP TABLE IF EXISTS `janus__subscription`;
+CREATE TABLE IF NOT EXISTS `janus__subscription` (
+  `sid` int(11) NOT NULL auto_increment,
+  `uid` int(11) NOT NULL,
+  `subscription` text NOT NULL,
+  `type` text,
+  `created` char(25) default NULL,
+  `ip` char(15) default NULL,
+  PRIMARY KEY  (`sid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `janus__tokens`
+--
+
+DROP TABLE IF EXISTS `janus__tokens`;
+CREATE TABLE IF NOT EXISTS `janus__tokens` (
+  `id` int(11) NOT NULL auto_increment,
+  `mail` varchar(320) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `notvalidafter` varchar(255) NOT NULL,
+  `usedat` varchar(255) default NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `token` (`token`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=32 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `janus__user`
+--
+
+DROP TABLE IF EXISTS `janus__user`;
+CREATE TABLE IF NOT EXISTS `janus__user` (
+  `uid` int(11) NOT NULL auto_increment,
+  `userid` text,
+  `type` text,
+  `email` varchar(320) default NULL,
+  `active` char(3) default 'yes',
+  `update` char(25) default NULL,
+  `created` char(25) default NULL,
+  `ip` char(15) default NULL,
+  `data` text,
+  PRIMARY KEY  (`uid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=37 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `janus__userData`
+--
+
+DROP TABLE IF EXISTS `janus__userData`;
+CREATE TABLE IF NOT EXISTS `janus__userData` (
+  `uid` int(11) NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `update` char(25) NOT NULL,
+  `created` char(25) NOT NULL,
+  `ip` char(15) NOT NULL,
+  UNIQUE KEY `uid` (`uid`,`key`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
