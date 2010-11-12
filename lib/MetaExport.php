a08ca07a1a6bf60932e01fbe685b4a246e14993b
Jacob Christiansen <jacob@colourbox.com>
2010-11-12 14:13:32 +0000

    Fixed issue 200 Changed the license to the MIT License.

--- lib/MetaExport.php
+++ lib/MetaExport.php
@@ -6,6 +6,7 @@
  * @package SimpleSAMLphp
  * @subpackeage JANUS
  * @version $Id$
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
  */
 class sspmod_janus_MetaExport
 {
--- lib/UIguard.php
+++ lib/UIguard.php
@@ -4,25 +4,12 @@
  *
  * PHP version 5
  *
- * JANUS is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * JANUS is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with JANUS. If not, see <http://www.gnu.org/licenses/>.
- *
  * @category   SimpleSAMLphp
  * @package    JANUS
  * @subpackage Core
  * @author     Jacob Christiansen <jach@wayf.dk>
  * @copyright  2009 Jacob Christiansen
- * @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT License
  * @version    SVN: $Id$
  * @link       http://code.google.com/p/janus-ssp/
  * @since      File available since Release 1.1.0
@@ -40,7 +27,7 @@
  * @subpackage Core
  * @author     Jacob Christiansen <jach@wayf.dk>
  * @copyright  2009 Jacob Christiansen
- * @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT License
  * @version    SVN: $Id$
  * @link       http://code.google.com/p/janus-ssp/
  * @since      Class available since Release 1.0.0
