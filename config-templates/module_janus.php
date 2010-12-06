d909fff5bcc3c787a90f249b7a2da6e3a5df057d
Jacob Christiansen <jacob@colourbox.com>
2010-12-06 11:25:14 +0000

    Removed unused options in configuration template

--- config-templates/module_janus.php
+++ config-templates/module_janus.php
@@ -16,69 +16,6 @@ $config = array(
     'auth' => 'mailtoken',
     'useridattr' => 'mail',
 
-    /**
-     * Mailtoken specific stuff
-     */
-
-    // Token lifetime in seconds
-    'token.lifetime' => 3600*24,
-
-    // Content of token mail
-    'email' => array(
-        'en' => array(
-            'body' => '
-                <html>
-                <head>
-                <title>JANUS token</title>
-                </head>
-                <body>
-                <p>To login to JANUS click the following link:</p>
-                <a href="%RETURNURL%?token=%TOKEN%&source=mailtoken">%RETURNURL%?token=%TOKEN%&source=mailtoken</a>
-                <p>If the link does not work, please try to copy the link
-                directly into your browsers address bar.</p>
-                <p>In case of problems contact the WAYF Secreteriat.</p>
-                <br />
-                <p>Best regards</p>
-                <p>WAYF Secreteriat</p>
-                <p>sekretariat@wayf.dk</p>
-                </body>
-                </html>',
-            'headers' => 'MIME-Version: 1.0' . "\r\n".
-                'Content-type: text/html; charset=iso-8859-1' . "\r\n".
-                'From: JANUS <no-reply@wayf.dk>' . "\r\n" .
-                'Reply-To: WAYF <sekretariatet@wayf.dk>' . "\r\n" .
-                'X-Mailer: PHP/' . phpversion(),
-            'subject' => 'JANUS: Login token',
-        ),
-        'da' => array(
-            'body' => '
-                <html>
-                <head>
-                <title>JANUS token</title>
-                </head>
-                <body>
-                <p>For at logge ind i JANUS, klik p&aring; linket:</p>
-                <a href="%RETUENURL%?token=%TOKEN%&source=mailtoken">%RETURNURL%?token=%TOKEN%&source=mailtoken</a>
-                <p>Hvis det ikke virker, pr&oslash;v at kopiere linket til
-                adressefeltet i din browser.</p>
-                <p>I tilf&aelig;lde af problemer med JANUS, kontakt WAYF
-                sekretariatet.</p>
-                <br />
-                <p>Venlig hilsen</p>
-                <p>WAYF sekretariatet</p>
-                <p>sekretariat@wayf.dk</p>
-                </body>
-                </html>
-            ',
-            'headers' => 'MIME-Version: 1.0' . "\r\n".
-                'Content-type: text/html; charset=iso-8859-1' . "\r\n".
-                'From: JANUS <no-reply@wayf.dk>' . "\r\n" .
-                'Reply-To: WAYF <sekretariatet@wayf.dk>' . "\r\n" .
-                'X-Mailer: PHP/' . phpversion(),
-            'subject' => 'JANUS: Login token',
-        ),
-    ),
-
     /*
      * Configuration for the database connection.
      */
@@ -119,11 +56,6 @@ $config = array(
      */
     'entity.useblacklist' => true,
     'entity.usewhitelist' => false,
-
-    /*
-     * Enable self user creation
-     */
-    'usercreation.allow' => true,
 
     /*
      * Default ARP
