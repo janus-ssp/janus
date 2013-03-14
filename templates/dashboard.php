b586a545565baa2890cbf3abffbb49242e319834
Lucas van Lierop <lucas.vanlierop@flex.surfnet.nl>
2013-03-14 14:08:24 +0100

    BACKLOG-845 Fix, main tab for admin is no longer closed by subtabs

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -1023,15 +1023,19 @@ if($this->data['uiguard']->hasPermission('admintab', null, $this->data['user']->
             echo '</table>';
         ?>
         </div>
-    </div>
-    <!-- ADMIN ENTITIES TAB END -->
-             <?php
-             }
-/* END SUBTAB ADMIN ENTITIES ******************************************************************************************/
-            ?>
+        <!-- ADMIN ENTITIES TAB END -->
+        <?php
+    }
+    /* END SUBTAB ADMIN ENTITIES ******************************************************************************************/
 
+
+
+    if (!$this->data['selectedSubTab']) {
+    ?>
+    </div>
 </div>
 <?php
+}
 }
 ?>
 
