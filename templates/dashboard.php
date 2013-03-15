06054e3015c9fa63554edb985acacaa8c3403884
Lucas van Lierop <lucas.vanlierop@flex.surfnet.nl>
2013-03-15 15:32:31 +0100

    BACKLOG-845 Fixed highlighting of subscription list

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -1214,9 +1214,6 @@ if (empty($this->data['selectedSubTab'])) {
                             $(this).css('background-color', '#FFFFFF');
                         }
                     );
-
-                    $("tr[id^='subscription_list_']:even").addClass("even");
-                    $("tr[id^='subscription_list_']:odd").addClass("odd");
                 });
             </script>
             <div id="inbox_menu">
@@ -1258,6 +1255,9 @@ if (empty($this->data['selectedSubTab'])) {
         ?>
         <div id="subscriptions">
             <script type="text/javascript">
+                $("tr[id^='subscription_list_']:even").addClass("even");
+                $("tr[id^='subscription_list_']:odd").addClass("odd");
+
                 function editSubscription(uid, sid) {
                     <?php
                     $select_types = '<option value="INBOX">Inbox</option>';
