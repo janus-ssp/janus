41112598d75c232c363cae0fb2298929a28c58a1
lucasvanlierop <lucas@vanlierop.org>
2013-09-13 15:53:47 +0200

    Issue 386 Added basic Doctrine ORM support

--- lib/DiContainer.php
+++ lib/DiContainer.php
@@ -1,10 +1,19 @@
 <?php
 require_once dirname(__DIR__) . "/autoload.php";
 
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+
 class sspmod_janus_DiContainer extends Pimple
 {
     const CONFIG = 'config';
+    const DB_PARAMS = 'dbParams';
     const METADATA_CONVERTER = 'metadata-converter';
+    const DOCTRINE_CACHE_DRIVER = 'doctrineCacheDriver';
+    const ENTITY_MANAGER = 'entityManager';
+    const ANNOTATION_DRIVER = 'annotationDriver';
 
     /** @var sspmod_janus_DiContainer */
     private static $instance;
@@ -12,7 +21,11 @@ class sspmod_janus_DiContainer extends Pimple
     public function __construct()
     {
         $this->registerConfig();
+        $this->registerDbParams();
         $this->registerMetadataConverter();
+        $this->registerDoctrineCacheDriver();
+        $this->registerEntityManager();
+        $this->registerAnnotationReader();
     }
 
     /**
@@ -45,6 +58,60 @@ class sspmod_janus_DiContainer extends Pimple
     }
 
     /**
+     * @return SimpleSAML_Configuration
+     */
+    public function getDbParams()
+    {
+        return $this[self::DB_PARAMS];
+    }
+
+    /**
+     * Parses db configuration into an array usable by doctrine
+     * This is mainly meant to parse legacy config.
+     *
+     * @return array
+     */
+    protected function registerDbParams()
+    {
+        $this[self::DB_PARAMS] = $this->share(function (sspmod_janus_DiContainer $container)
+        {
+            $dbParams = $container->getConfig()->getArray('store');
+
+            // Doctrine uses user instead of username
+            if (isset($dbParams['username'])) {
+                $dbParams['user'] = $dbParams['username'];
+                unset($dbParams['username']);
+            }
+
+            // Doctrine does not use dsn
+            if (isset($dbParams['dsn'])) {
+
+                $dsnParts = preg_split('/[:;]/', $dbParams['dsn']);
+                unset($dbParams['dsn']);
+
+                // Set driver (always use pdo)
+                $dbParams['driver'] = 'pdo_' . array_shift($dsnParts);
+
+                // Set host, dbname etc.
+                foreach ($dsnParts as $value) {
+                    if (empty($value)) {
+                        continue;
+                    }
+
+                    $entryParts = explode('=', $value);
+                    if (count($entryParts) === 1) {
+                        $dbParams[$entryParts[0]] = true;
+                    } else {
+                        $dbParams[$entryParts[0]] = $entryParts[1];
+                    }
+                }
+            }
+
+            return $dbParams;
+        });
+    }
+    
+    /**
      * @return sspmod_janus_Metadata_Converter_Converter
      */
     public function getMetaDataConverter()
@@ -70,6 +137,119 @@ class sspmod_janus_DiContainer extends Pimple
                 $metadataConverter->registerCommand($mapKeysCommand);
 
                 return $metadataConverter;
+            }
+        );
+    }
+
+    /**
+     * @return \Doctrine\Common\Cache\CacheProvider
+     */
+    public function getDoctrineCacheDriver()
+    {
+        return $this[self::DOCTRINE_CACHE_DRIVER];
+    }
+
+    protected function registerDoctrineCacheDriver()
+    {
+        $this[self::DOCTRINE_CACHE_DRIVER] = function (sspmod_janus_DiContainer $container)
+        {
+            // @todo base this on config
+            $isDevMode = false;
+
+            // @todo get caching type from config instead of using $isDevMode
+            // Configure caching
+            if ($isDevMode) {
+                $cacheDriver = new \Doctrine\Common\Cache\ArrayCache();
+            } elseif(extension_loaded('apc') && ini_get('apc.enabled')) {
+                $cacheDriver = new \Doctrine\Common\Cache\ApcCache();
+            } elseif (class_exists('Memcache')) {
+                $memcache = new Memcache();
+                $memcache->connect('localhost', 11211);
+                $cacheDriver = new \Doctrine\Common\Cache\MemcacheCache();
+                $cacheDriver->setMemcache($memcache);
+            } else {
+                $cacheDriver = new \Doctrine\Common\Cache\FilesystemCache(sys_get_temp_dir());
+            }
+
+            return $cacheDriver;
+        };
+    }
+
+    /** @return EntityManager */
+    public function getEntityManager()
+    {
+        return $this[self::ENTITY_MANAGER];
+    }
+
+    protected function registerEntityManager()
+    {
+        $this[self::ENTITY_MANAGER] = $this->share(function (sspmod_janus_DiContainer $container)
+        {
+            $config = $container->getConfig();
+
+            // @todo base this on config
+            $isDevMode = true;
+
+            $doctrineConfig = new \Doctrine\ORM\Configuration();
+
+            $cacheDriver = $container->getDoctrineCacheDriver();
+            $doctrineConfig->setMetadataCacheImpl($cacheDriver);
+            $doctrineConfig->setQueryCacheImpl($cacheDriver);
+            $doctrineConfig->setResultCacheImpl($cacheDriver);
+
+            // Configure Proxy class generation
+            $doctrineConfig->setAutoGenerateProxyClasses((bool) !$isDevMode);
+            // @todo get from config
+            $doctrineConfig->setProxyDir('tmp');
+            $doctrineConfig->setProxyNamespace('Proxies');
+
+            // Configure annotation reader
+            $annotationReader = $container->getAnnotationReader();
+            $paths = array(JANUS_ROOT_DIR  . "/lib/Model");
+            $driverImpl =  new AnnotationDriver($annotationReader, $paths);
+            $doctrineConfig->setMetadataDriverImpl($driverImpl);
+
+            $dbParams = $container->getDbParams();
+
+            $entityManager = EntityManager::create($dbParams, $doctrineConfig, $eventManager);
+
+            return $entityManager;
+        });
+    }
+
+    /**
+     * @return \Doctrine\Common\Annotations\AnnotationReader
+     */
+    public function getAnnotationReader()
+    {
+        return $this[self::ANNOTATION_DRIVER];
+    }
+
+    /**
+     * Creates annotation reader
+     *
+     * @return Doctrine\Common\Annotations\CachedReader
+     */
+    protected function registerAnnotationReader()
+    {
+        $this[self::ANNOTATION_DRIVER] = $this->share(
+            function (sspmod_janus_DiContainer $container)
+            {
+                $annotationReader = new AnnotationReader();
+
+                AnnotationRegistry::registerFile(VENDOR_DIR . '/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
+
+                // @todo enable caching
+                $cacheDriver = $container->getDoctrineCacheDriver();
+                $cacheDriver->setNamespace('doctrine-annotation-cache');
+
+                $annotationReader = new \Doctrine\Common\Annotations\CachedReader(
+                    $annotationReader,
+                    $cacheDriver,
+                    false
+                );
+
+                return $annotationReader;
             }
         );
     }
