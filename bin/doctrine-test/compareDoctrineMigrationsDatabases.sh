eb2d3288065544fcefb6d4690a24a895c7b5cd77
lucasvanlierop <lucas@vanlierop.org>
2013-09-16 13:36:02 +0200

    Issue 386 Added some info comments

--- bin/doctrine-test/compareDoctrineMigrationsDatabases.sh
+++ bin/doctrine-test/compareDoctrineMigrationsDatabases.sh
@@ -20,11 +20,13 @@ echo 'create database janus_wayf CHARSET=utf8 COLLATE=utf8_unicode_ci' | mysql -
 mysql -uroot -p janus_wayf < docs/janus.sql
 mysqldump -uroot -p --no-data janus_wayf > /tmp/janus_wayf.sql
 
-#ignore unimportant text differences
+#ignore unimportant text differences, Docrine creates larger text fields by default, these cause only  little overhead,
+# can be changed back if really required, not really important for janus since it will not contain many records
 sed -i 's/ mediumtext/ longtext/' /tmp/janus_wayf.sql
 sed -i 's/ text/ longtext/' /tmp/janus_wayf.sql
 
-# Remove collations to reduce diff
+# Remove collations to reduce diff, all tables are utf8 anyway and collation will be changed to unicode, which is a good thing:
+# http://forums.mysql.com/read.php?103,187048,188748#msg-188748
 sed -i 's/ COLLATE utf8_unicode_ci//' /tmp/janus_wayf.sql
 sed -i 's/ COLLATE utf8_unicode_ci//' /tmp/janus_migrations_test.sql
 sed -i 's/ COLLATE=utf8_unicode_ci//' /tmp/janus_migrations_test.sql
