345b6bfb2c3076c5e2f2951ff2afd8f91234b2f6
lucasvanlierop <lucas@vanlierop.org>
2013-09-17 15:56:39 +0200

    Issue 386 Corrected initial version nr

--- bin/update.sh
+++ bin/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Skip initial migration which creates the schema as it was when doctrine migrations was introduced
-./bin/doctrine migrations:version --add 20130714185021 > /dev/null 2>&1
+./bin/doctrine migrations:version --add 0 > /dev/null 2>&1
 
 # Run remaining migrations
 ./bin/doctrine migrations:migrate --no-interaction
