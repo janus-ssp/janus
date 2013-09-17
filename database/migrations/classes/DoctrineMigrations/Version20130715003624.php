970604ffa71aa2cec213a808034d6bcdab5b1f19
lucasvanlierop <lucas@vanlierop.org>
2013-09-17 15:53:29 +0200

    Issue 386 kept remoteentity id out of primary key since it is too big

--- database/migrations/classes/DoctrineMigrations/Version20130715003624.php
+++ database/migrations/classes/DoctrineMigrations/Version20130715003624.php
@@ -20,7 +20,9 @@ class Version20130715003624 extends AbstractMigration
         $entityBlockedEntityRelationTable->setPrimaryKey(array('eid', 'revisionid', 'remoteeid'));
 
         $entityDisableConsentRelationTable = $schema->getTable($this->tablePrefix . 'disableConsent');
-        $entityDisableConsentRelationTable->setPrimaryKey(array('eid', 'revisionid', 'remoteentityid'));
+        // Key column is currently too long to be part of primary key so it has to be shortened first
+        // It would be even better to fix this by using eid instead of entityid https://github.com/janus-ssp/janus/issues/389
+        $entityDisableConsentRelationTable->setPrimaryKey(array('eid', 'revisionid'));
 
         $allowedEntityTable = $schema->getTable($this->tablePrefix . 'allowedEntity');
         $allowedEntityTable->setPrimaryKey(array('eid', 'revisionid', 'remoteeid'));
