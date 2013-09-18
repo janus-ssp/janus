47bc2897d473b6ba5d0b5acae90ef67d355fc547
lucasvanlierop <lucas@vanlierop.org>
2013-09-18 11:45:25 +0200

    Issue 386 Organized codeblocks by indenting

--- bin/doctrine-test/compareDoctrineMigrationsDatabases.sh
+++ bin/doctrine-test/compareDoctrineMigrationsDatabases.sh
@@ -6,14 +6,14 @@ MYSQLDUMP_BIN="mysqldump --defaults-extra-file=$HOME/my.cnf"
 # NOTE: before running this, change your database name to: 'janus_migrations_test'
 
 echo "Importing doctrine export"
-# Create new database from doctrine model
-echo 'drop database janus_migrations_test'  | $MYSQL_BIN
-echo 'create database janus_migrations_test CHARSET=utf8 COLLATE=utf8_unicode_ci'  | $MYSQL_BIN
-# Comment following line to test installing instead of upgrading
-#$MYSQL_BIN janus_migrations_test < docs/janus.sql
-./bin/doctrine migrations:migrate --no-interaction
+    # Create new database from doctrine model
+    echo 'drop database janus_migrations_test'  | $MYSQL_BIN
+    echo 'create database janus_migrations_test CHARSET=utf8 COLLATE=utf8_unicode_ci'  | $MYSQL_BIN
+    # Comment following line to test installing instead of upgrading
+    #$MYSQL_BIN janus_migrations_test < docs/janus.sql
+    ./bin/doctrine migrations:migrate --no-interaction
 
-$MYSQLDUMP_BIN --no-data janus_migrations_test > /tmp/janus_migrations_test.sql
+    $MYSQLDUMP_BIN --no-data janus_migrations_test > /tmp/janus_migrations_test.sql
 
 echo "Check differences between migrations and schematool, there should be none otherwise the models do not map to the db"
     echo 'drop database janus_schematool_test'  | $MYSQL_BIN
@@ -32,36 +32,35 @@ echo "Check differences between migrations and schematool, there should be none 
     colordiff -u /tmp/janus_migrations_test.sql /tmp/janus_schematool_test-dump.sql
 
 echo "Importing Janus sql"
-echo 'drop database janus_wayf'  | $MYSQL_BIN
-echo 'create database janus_wayf CHARSET=utf8 COLLATE=utf8_unicode_ci'  | $MYSQL_BIN
-$MYSQL_BIN janus_wayf < docs/janus.sql
-$MYSQLDUMP_BIN --no-data janus_wayf > /tmp/janus_wayf.sql
+    echo 'drop database janus_wayf'  | $MYSQL_BIN
+    echo 'create database janus_wayf CHARSET=utf8 COLLATE=utf8_unicode_ci'  | $MYSQL_BIN
+    $MYSQL_BIN janus_wayf < docs/janus.sql
+    $MYSQLDUMP_BIN --no-data janus_wayf > /tmp/janus_wayf.sql
 
 #ignore unimportant text differences, Docrine creates larger text fields by default, these cause only  little overhead,
-# can be changed back if really required, not really important for janus since it will not contain many records
-sed -i 's/ mediumtext/ longtext/' /tmp/janus_wayf.sql
-sed -i 's/ text/ longtext/' /tmp/janus_wayf.sql
+    # can be changed back if really required, not really important for janus since it will not contain many records
+    sed -i 's/ mediumtext/ longtext/' /tmp/janus_wayf.sql
+    sed -i 's/ text/ longtext/' /tmp/janus_wayf.sql
 
 # Remove collations to reduce diff, all tables are utf8 anyway and collation will be changed to unicode, which is a good thing:
 # http://forums.mysql.com/read.php?103,187048,188748#msg-188748
-sed -i 's/ COLLATE utf8_unicode_ci//' /tmp/janus_wayf.sql
-sed -i 's/ COLLATE utf8_unicode_ci//' /tmp/janus_migrations_test.sql
-sed -i 's/ COLLATE=utf8_unicode_ci//' /tmp/janus_migrations_test.sql
+    sed -i 's/ COLLATE utf8_unicode_ci//' /tmp/janus_wayf.sql
+    sed -i 's/ COLLATE utf8_unicode_ci//' /tmp/janus_migrations_test.sql
+    sed -i 's/ COLLATE=utf8_unicode_ci//' /tmp/janus_migrations_test.sql
 
 # Remove comma's to reduce diff
-sed -i 's/,$//' /tmp/janus_wayf.sql
-sed -i 's/,$//' /tmp/janus_migrations_test.sql
+    sed -i 's/,$//' /tmp/janus_wayf.sql
+    sed -i 's/,$//' /tmp/janus_migrations_test.sql
 
 colordiff -u /tmp/janus_wayf.sql /tmp/janus_migrations_test.sql
 
 echo "Test reverse migration"
+    ./bin/doctrine migrations:migrate --no-interaction 0
 
-./bin/doctrine migrations:migrate --no-interaction 0
+    $MYSQLDUMP_BIN --no-data janus_migrations_test > /tmp/janus_migrations_test.sql
+    sed -i 's/ COLLATE utf8_unicode_ci//' /tmp/janus_migrations_test.sql
+    sed -i 's/ COLLATE=utf8_unicode_ci//' /tmp/janus_migrations_test.sql
 
-$MYSQLDUMP_BIN --no-data janus_migrations_test > /tmp/janus_migrations_test.sql
-sed -i 's/ COLLATE utf8_unicode_ci//' /tmp/janus_migrations_test.sql
-sed -i 's/ COLLATE=utf8_unicode_ci//' /tmp/janus_migrations_test.sql
+    $MYSQLDUMP_BIN --no-data janus_wayf > /tmp/janus_wayf.sql
 
-$MYSQLDUMP_BIN --no-data janus_wayf > /tmp/janus_wayf.sql
-
-colordiff -u /tmp/janus_wayf.sql /tmp/janus_migrations_test.sql
+    colordiff -u /tmp/janus_wayf.sql /tmp/janus_migrations_test.sql
