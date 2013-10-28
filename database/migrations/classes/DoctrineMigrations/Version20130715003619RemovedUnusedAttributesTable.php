645981361bcd39900b7afb98c313b9cc56a2f6bf
lucasvanlierop <lucas@vanlierop.org>
2013-10-28 13:25:25 +0100

    #388 Added database patch for removing obsolete attribute table

---
+++ database/migrations/classes/DoctrineMigrations/Version20130715003619RemovedUnusedAttributesTable.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema,
+    Doctrine\DBAL\Types\Type;
+
+/**
+ * Class Version20130715003619RemovedUnusedAttributesTable
+ * @package DoctrineMigrations
+ */
+class Version20130715003619RemovedUnusedAttributesTable extends AbstractMigration
+{
+    private $tablePrefix = 'janus__';
+
+    /**
+     * Attributes table was not used in the code anymore so can be removed
+     *
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $prefixedTableName = $this->tablePrefix . 'attribute';
+        if ($schema->hasTable($prefixedTableName)) {
+            $schema->dropTable($prefixedTableName);
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     *
+     * Just here to test reverse migration
+     */
+    public function down(Schema $schema)
+    {
+        $this->addSql("
+            CREATE TABLE janus__attribute (
+              eid int(11) NOT NULL,
+              revisionid int(11) NOT NULL,
+              `key` text NOT NULL,
+              `value` text NOT NULL,
+              created char(25) NOT NULL,
+              ip char(39) NOT NULL
+            ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+        ");
+    }
+}
