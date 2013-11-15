6b47b9c98dbef08fe404c90830425cf5ee8ed18e
oharsta <oharsta@zilverline.com>
2013-11-15 13:01:31 +0100

    Added NULL array for empty ARP

--- database/migrations/classes/DoctrineMigrations/Version20131114150840RemoveArpToEntityRevision.php
+++ database/migrations/classes/DoctrineMigrations/Version20131114150840RemoveArpToEntityRevision.php
@@ -21,6 +21,10 @@ class Version20131114150840RemoveArpToEntityRevision extends AbstractMigration
               (select `attributes` from `" . DB_TABLE_PREFIX . "arp` ARP where ARP.`aid` = ENTITY_REVISION.`arp` )
         ");
         $this->addSql("
+            UPDATE `" . DB_TABLE_PREFIX . "entityRevision` ENTITY_REVISION SET `arp_attributes` = 'N;'
+              where ENTITY_REVISION.`arp_attributes` IS NULL
+        ");
+        $this->addSql("
             ALTER TABLE `" . DB_TABLE_PREFIX . "entityRevision` DROP FOREIGN KEY `FK_72BCD7F2FB58124D`
         ");
         $this->addSql("
