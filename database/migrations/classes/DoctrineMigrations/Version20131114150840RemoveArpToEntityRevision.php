066d2d7a84411666f48c7f2041268dcb59cf1349
oharsta <oharsta@zilverline.com>
2013-11-18 09:44:50 +0100

    Changed database migration to be compatible with the ORM database changes

--- database/migrations/classes/DoctrineMigrations/Version20131114150840RemoveArpToEntityRevision.php
+++ database/migrations/classes/DoctrineMigrations/Version20131114150840RemoveArpToEntityRevision.php
@@ -13,25 +13,25 @@ class Version20131114150840RemoveArpToEntityRevision extends AbstractMigration
     public function up(Schema $schema)
     {
         $this->addSql("
-            ALTER TABLE `" . DB_TABLE_PREFIX . "entityRevision`
+            ALTER TABLE `" . DB_TABLE_PREFIX . "connectionRevision`
               ADD `arp_attributes` text COMMENT '(DC2Type:array)'
         ");
         $this->addSql("
-            UPDATE `" . DB_TABLE_PREFIX . "entityRevision` ENTITY_REVISION SET `arp_attributes` =
-              (select `attributes` from `" . DB_TABLE_PREFIX . "arp` ARP where ARP.`aid` = ENTITY_REVISION.`arp` )
+            UPDATE `" . DB_TABLE_PREFIX . "connectionRevision` CONNECTION_REVISION SET `arp_attributes` =
+              (select `attributes` from `" . DB_TABLE_PREFIX . "arp` ARP where ARP.`aid` = CONNECTION_REVISION.`arp` )
         ");
         $this->addSql("
-            UPDATE `" . DB_TABLE_PREFIX . "entityRevision` ENTITY_REVISION SET `arp_attributes` = 'N;'
-              where ENTITY_REVISION.`arp_attributes` IS NULL
+            UPDATE `" . DB_TABLE_PREFIX . "connectionRevision` CONNECTION_REVISION SET `arp_attributes` = 'N;'
+              where CONNECTION_REVISION.`arp_attributes` IS NULL
         ");
         $this->addSql("
-            ALTER TABLE `" . DB_TABLE_PREFIX . "entityRevision` DROP FOREIGN KEY `FK_72BCD7F2FB58124D`
+            ALTER TABLE `" . DB_TABLE_PREFIX . "connectionRevision` DROP FOREIGN KEY `FK_72BCD7F2FB58124D`
         ");
         $this->addSql("
-            ALTER TABLE `" . DB_TABLE_PREFIX . "entityRevision` DROP INDEX `IDX_72BCD7F2FB58124D`
+            ALTER TABLE `" . DB_TABLE_PREFIX . "connectionRevision` DROP INDEX `IDX_72BCD7F2FB58124D`
         ");
         $this->addSql("
-            ALTER TABLE `" . DB_TABLE_PREFIX . "entityRevision` DROP COLUMN `arp`
+            ALTER TABLE `" . DB_TABLE_PREFIX . "connectionRevision` DROP COLUMN `arp`
         ");
         $this->addSql("
             DROP TABLE `" . DB_TABLE_PREFIX . "arp`
