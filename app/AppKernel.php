96dcf49232b1655487b9db72dfe0e7b05aec61f0
lucasvanlierop <lucas@vanlierop.org>
2013-11-25 14:27:19 +0100

    #396 Added WIP janus connections bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
-            new FOS\RestBundle\FOSRestBundle()
+            new FOS\RestBundle\FOSRestBundle(),
+            new Janus\ConnectionsBundle\JanusConnectionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Janus/ConnectionsBundle/JanusConnectionsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Janus\ConnectionsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JanusConnectionsBundle extends Bundle
+{
+}
