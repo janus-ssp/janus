e2cd667cc35ec0b1616c972530fad895e6a6c9ef
lucasvanlierop <lucas@vanlierop.org>
2014-01-20 17:21:25 +0100

    #396 Added missing config loader

--- src/Janus/ServiceRegistryBundle/DependencyInjection/JanusServiceRegistryExtension.php
+++ src/Janus/ServiceRegistryBundle/DependencyInjection/JanusServiceRegistryExtension.php
@@ -3,6 +3,7 @@
 namespace Janus\ServiceRegistryBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
@@ -11,7 +12,11 @@ class JanusServiceRegistryExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $configDir = __DIR__ . '/../Resources/config';
+        $loader = new YamlFileLoader($container, new FileLocator($configDir));
+        $env = $container->getParameter("kernel.environment");
+        $loader->load($configDir . '/parameters_' . $env . '.yml');
+        $loader = new XmlFileLoader($container, new FileLocator($configDir));
         $loader->load('services.xml');
     }
 
