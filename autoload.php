fb8075a4a2269e03000e4b177aaec6f7ea0a21b8
lucasvanlierop <lucas@vanlierop.org>
2014-01-27 14:44:59 +0100

    #396 Improved autodetecting which vendor code should be used

--- autoload.php
+++ autoload.php
@@ -1,21 +1,74 @@
 <?php
 define("JANUS_ROOT_DIR", __DIR__);
+return requireLoaders(JANUS_ROOT_DIR);
 
-// Since janus can be installed both in the modules dir of simplesaml or in vendor dir of a different project
-// Find out if janus has it's own vendor dir or is installed as composer dependency of a project
-$projectVendorDir = realpath(JANUS_ROOT_DIR . '/../../../vendor');
-define('IS_JANUS_INSTALLED_AS_COMPOSER_DEPENDENCY', is_dir($projectVendorDir));
+/**
+ * Loads Composer and SimpleSampPhp autoloaders.
+ *
+ * @param string $janusRootDir
+ * @return mixed
+ */
+function requireLoaders($janusRootDir)
+{
+    /** Since janus can be installed both in the modules dir of simplesaml
+     * or in vendor dir of a main project some 'magic' is required to find fix autoloading
+     */
+    $vendorDir = findVendorDir($janusRootDir);
 
-if (IS_JANUS_INSTALLED_AS_COMPOSER_DEPENDENCY) {
-    define("VENDOR_DIR", $projectVendorDir);
-    define("SIMPLESAMLPHP_DIR", VENDOR_DIR .   '/simplesamlphp/simplesamlphp');
-} else {
-    define("VENDOR_DIR", JANUS_ROOT_DIR . '/vendor');
-    define("SIMPLESAMLPHP_DIR", realpath(JANUS_ROOT_DIR . '/../../'));
+    /**
+     * When SimpleSamlPhp is not installed using composer it's own custom autoloader needs to be
+     * required
+     */
+    if (!isSimpleSamlPhpInstalledInVendor($vendorDir)) {
+        $simpleSamlPhpDir = realpath($janusRootDir . '/../../../');
+        require_once $simpleSamlPhpDir . "/lib/_autoload.php";
+    }
+
+    // Load Composer autoloader
+    $composerAutoLoader = require $vendorDir . "/autoload.php";
+    return $composerAutoLoader;
 }
 
-// Load SimpleSamlPhp Autoloader
-require_once SIMPLESAMLPHP_DIR . "/lib/_autoload.php";
+/**
+ * Checks if janus has it's own vendor dir,.
+ *
+ * This is the case when:
+ * - running an install from archive
+ * - developing on Janus API
+ * - running tests on CI server
+ *
+ * @param string $janusRootDir
+ * @return string
+ */
+function findVendorDir($janusRootDir)
+{
+    $janusVendorDir = $janusRootDir . '/vendor';
+    if (is_dir($janusVendorDir)) {
+        return $janusVendorDir;
+    }
 
-// Load Composer autoloader
-return require_once VENDOR_DIR . "/autoload.php";
+    /**
+     * Find out if janus is installed as composer dependency of a main project
+     * This is the case when using sample as part of a bigger project like for
+     * example the OpenConext ServiceRegistry
+     */
+    $mainProjectVendorDir = realpath($janusRootDir . '/../../../vendor');
+    if (is_dir($mainProjectVendorDir)) {
+        return $mainProjectVendorDir;
+    }
+}
+
+/**
+ * Checks if SimpleSamlPhp is installed in the vendor dir using composer
+ *
+ * Note that it's also possible that a vendor dir exists within janus without SimpleSamlPhp
+ * This is the case when installing from archive.
+ *
+ * @param string $vendorDir
+ * @return bool
+ */
+function isSimpleSamlPhpInstalledInVendor($vendorDir)
+{
+    $simpleSamlPhpDirInVendor = $vendorDir . '/simplesamlphp/simplesamlphp/';
+    return is_dir($simpleSamlPhpDirInVendor);
+}
