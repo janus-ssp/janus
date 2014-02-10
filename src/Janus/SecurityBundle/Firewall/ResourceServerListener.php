5a0ed3daee2c4c250a8921c62165eba7a058f8f1
lucasvanlierop <lucas@vanlierop.org>
2014-02-10 12:06:44 +0100

    #396 Updated comment

--- src/Janus/SecurityBundle/Firewall/ResourceServerListener.php
+++ src/Janus/SecurityBundle/Firewall/ResourceServerListener.php
@@ -50,11 +50,12 @@ class ResourceServerListener implements ListenerInterface
      *
      * @param GetResponseEvent $event
      * @return array
+     * @todo inject this?
      */
     private function getHeadersFromEvent(GetResponseEvent $event)
     {
-        // @todo inject this?
         $headers = $event->getRequest()->headers->all();
+
         // @todo find what exactly causes this and if a better fix can be created
         // Apache removes AUTHORIZATION HEADER so headers need to be requested directly from the server.
         if (function_exists('apache_request_headers')) {
