88ddfbf907b0ce46b4f7f5024cc2ad82146779ab
lucasvanlierop <lucas@vanlierop.org>
2014-02-17 13:23:56 +0100

    #396 Moved adding listener to oauthclient bundle itself

--- src/Janus/OauthClientBundle/JanusOauthClientBundle.php
+++ src/Janus/OauthClientBundle/JanusOauthClientBundle.php
@@ -3,7 +3,16 @@
 namespace Janus\OauthClientBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Janus\OauthClientBundle\DependencyInjection\Security\Factory\ResourceServerSecurityFactory;
 
 class JanusOauthClientBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new ResourceServerSecurityFactory());
+    }
 }
--- src/Janus/ServiceRegistryBundle/JanusServiceRegistryBundle.php
+++ src/Janus/ServiceRegistryBundle/JanusServiceRegistryBundle.php
@@ -2,20 +2,11 @@
 /**
  * @author Lucas van Lierop <lucas@vanlierop.org>
  */
-
 namespace Janus\ServiceRegistryBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Janus\OauthClientBundle\DependencyInjection\Security\Factory\ResourceServerSecurityFactory;
 
 class JanusServiceRegistryBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $extension = $container->getExtension('security');
-        $extension->addSecurityListenerFactory(new ResourceServerSecurityFactory());
-    }
 }
