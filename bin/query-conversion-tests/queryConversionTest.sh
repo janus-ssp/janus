4eb5951721e88543ccc7442692efd929ada6806b
lucasvanlierop <lucas@vanlierop.org>
2014-02-24 15:37:06 +0100

    #396 Formatted and annotated query

--- bin/query-conversion-tests/queryConversionTest.sh
+++ bin/query-conversion-tests/queryConversionTest.sh
@@ -2,15 +2,28 @@
 MYSQL_BIN="mysql --defaults-extra-file=$HOME/my.cnf janus_migrations_test"
 
 # Correlate entities with current metadata
-$MYSQL_BIN <<SQL
-    SELECT cr.eid,cr.revisionid,cr.allowedall,cr.entityid,m1.value AS name
+#$MYSQL_BIN
+<<SQL
+    SELECT
+        cr.eid,
+        cr.revisionid,
+        cr.allowedall,
+        cr.entityid,
+        m1.value AS name
+    -- Select all connections
     FROM janus__connection AS c
+
+    -- Join latest (production ready) revisions
     INNER JOIN janus__connectionRevision AS cr
         ON  cr.eid = c.id
         AND cr.revisionid = c.revisionNr
+        AND cr.state="prodaccepted"
+
+    -- Join metadata for latest revisions
     LEFT JOIN janus__metadata AS m1
         ON m1.connectionRevisionId = cr.id
         AND m1.key="name:en"
-    WHERE cr.state="prodaccepted"
+
+    -- Order by connection id (== eid)
     ORDER BY c.id
 SQL
