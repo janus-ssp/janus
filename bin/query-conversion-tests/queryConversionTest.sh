16a337c929365ee1fdf2ccd3b769356c3887c45b
lucasvanlierop <lucas@vanlierop.org>
2014-02-24 10:28:46 +0100

    #396 Removed need for revisionid subquery

--- bin/query-conversion-tests/queryConversionTest.sh
+++ bin/query-conversion-tests/queryConversionTest.sh
@@ -4,13 +4,13 @@ MYSQL_BIN="mysql --defaults-extra-file=$HOME/my.cnf janus_migrations_test"
 # Correlate entities with current metadata
 $MYSQL_BIN <<SQL
     SELECT cr.eid,cr.revisionid,cr.allowedall,cr.entityid,m1.value AS name
-    FROM janus__connectionRevision AS cr
+    FROM janus__connection AS c
+    INNER JOIN janus__connectionRevision AS cr
+        ON  cr.eid = c.id
+        AND cr.revisionid = c.revisionNr
     LEFT JOIN janus__metadata AS m1
-    ON m1.connectionRevisionId = cr.id
-    AND m1.key="name:en"
-    WHERE cr.revisionid = (
-    SELECT MAX(revisionid) FROM janus__connectionRevision AS e2 WHERE cr.eid=e2.eid
-    )
-    AND cr.state="prodaccepted"
-    ORDER BY cr.eid
+        ON m1.connectionRevisionId = cr.id
+        AND m1.key="name:en"
+    WHERE cr.state="prodaccepted"
+    ORDER BY c.id
 SQL
