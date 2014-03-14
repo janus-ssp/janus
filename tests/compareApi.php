2844661555001d5655690a489f004dac6c97a2c0
lucasvanlierop <lucas@vanlierop.org>
2014-03-14 13:42:43 +0100

    #420 Checked each call for every SP and IDP and converted test to class for this

--- tests/compareApi.php
+++ tests/compareApi.php
@@ -14,117 +14,193 @@
 
 require __DIR__ . "/../app/autoload.php";
 
-$defaultArguments = array(
-    'rest' => 1,
-    'user_id' => 'engine',
-    'janus_key' => 'engine'
-);
+$test = new OldApiTest();
+$test->testSps();
+$test->testIds();
+echo 'end';
 
-$methods = array(
-    'getSpList' => array(),
-    'getIdpList' => array(),
-    'getAllowedIdps' => array(
-        'spentityid' => 'https://profile.surfconext.nl/simplesaml/module.php/saml/sp/metadata.php/default-sp'
-    ),
-    'getEntity' => array(
-        'entityid' => 'https://surfguest.nl/test'
-    ),
-    'isConnectionAllowed' => array(
-        'spentityid' => 'https://profile.surfconext.nl/simplesaml/module.php/saml/sp/metadata.php/default-sp',
-        'idpentityid' => 'https://surfguest.nl/test'
-
-    ),
-    'arp' => array(
-        'entityid' => 'https://profile.surfconext.nl/simplesaml/module.php/saml/sp/metadata.php/default-sp'
-    ),
-    'getEntity' => array(
-        'entityid' => 'https://profile.surfconext.nl/simplesaml/module.php/saml/sp/metadata.php/default-sp'
-    ),
-    'getUser' => array(
-        'userid' => 'admin'
-    ),
-    'getMetadata' => array(
-        'entityid' => 'https://engine.surfconext.nl/authentication/sp/metadata'
-    ),
-    'getAllowedSps' => array(
-        'idpentityid' => 'https://surfguest.nl/test'
-    ),
-    'findIdentifiersByMetadata' => array(
-        'key' => 'name:en',
-        'value' => 'e',
-        'userid' => 'admin'
-    )
-);
-
-$oldHttpClient = new \Guzzle\Http\Client(
-    'https://serviceregistry.demo.openconext.org/simplesaml/module.php/janus/services/rest/'
-);
-$newHttpClient = new \Guzzle\Http\Client(
-    'https://serviceregistry-janus-1.16.demo.openconext.org/simplesaml/module.php/janus/services/rest/'
-);
-
-foreach ($methods as $method => $methodArguments) {
-    $arguments['method'] = $method;
-    $arguments = array_merge($arguments, $defaultArguments, $methodArguments);
-
-    echo "\n\ncalling old {$method}\n";
-
-    try {
-        $oldResponse = createResponse($oldHttpClient, $arguments);
-    } catch (Exception $ex) {
-        echo "Failed " . PHP_EOL .
-            $ex->getMessage() . PHP_EOL;
-        break;
-    }
-
-    echo "\n\ncalling new {$method}\n";
-
-    try {
-        $newResponse = createResponse($newHttpClient, $arguments);
-    } catch (Exception $ex) {
-        echo "Failed " . PHP_EOL .
-            $ex->getMessage() . PHP_EOL;
-        break;
-    }
-
-    $diff = array_diff_recursive($oldResponse->json(), $newResponse->json());
-    if (!empty($diff)) {
-        var_dump($diff);
-    }
-}
-
-function createResponse(\Guzzle\Http\Client $client, array $arguments)
+class OldApiTest
 {
-    $request = $client->get('', array(), array(
-        'query' => $arguments
-    ));
-    $request->getCurlOptions()->set(CURLOPT_SSL_VERIFYHOST, false);
-    $request->getCurlOptions()->set(CURLOPT_SSL_VERIFYPEER, false);
+    private $defaultArguments = array(
+        'rest' => 1,
+        'user_id' => 'engine',
+        'janus_key' => 'engine'
+    );
 
-    return $request->send();
-}
+//    'getIdpList' => array(),
+//'getUser' => array(
+//    'userid' => 'admin'
+//),
 
-function array_diff_recursive($array1, $array2)
-{
-    $diff = array();
-    foreach ($array1 as $key => $value) {
-        if (array_key_exists($key, $array2)) {
-            if (is_array($value)) {
-                $subDiff = array_diff_recursive($value, $array2[$key]);
-                if (count($subDiff)) {
-                    $diff[$key] = $subDiff;
-                }
-            } else {
-                if ($value != $array2[$key]) {
-                    $diff[$key] = $value;
-                }
-            }
-        } else {
-            $diff[$key] = $value;
+    private $genericMethods = array(
+        'getEntity' => array(
+            'entityid' => 'entityid'
+        )
+    );
+
+    private $idpMethods = array(
+        'getAllowedSps' => array(
+            'idpentityid' => 'entityid'
+        )
+    );
+
+    private $spMethods = array(
+        'getAllowedIdps' => array(
+            'spentityid' => 'entityid'
+        ),
+        'isConnectionAllowed' => array(
+            'spentityid' => 'entityid',
+            'idpentityid' => 'https://surfguest.nl/test'
+
+        ),
+        'arp' => array(
+            'entityid' => 'entityid'
+        ),
+        'getMetadata' => array(
+            'entityid' => 'entityid'
+        ),
+        'findIdentifiersByMetadata' => array(
+            'key' => 'name:en',
+            'value' => 'e',
+            'userid' => 'admin'
+        )
+    );
+
+    /**
+     * @var \Guzzle\Http\Client
+     */
+    private $oldHttpClient;
+
+    /**
+     * @var Guzzle\Http\Client
+     */
+    private $newHttpClient;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->oldHttpClient = new \Guzzle\Http\Client(
+            'https://serviceregistry.demo.openconext.org/simplesaml/module.php/janus/services/rest/'
+        );
+        $this->newHttpClient = new \Guzzle\Http\Client(
+            'https://serviceregistry-janus-1.16.demo.openconext.org/simplesaml/module.php/janus/services/rest/'
+        );
+    }
+
+    // Exec all methods for each SP
+    public function testSps()
+    {
+        $spMethods = array_merge($this->genericMethods, $this->spMethods);
+        $spListResponse = $this->createResponse($this->oldHttpClient, array_merge(
+//            array('method' => 'getSpList'),
+            $this->defaultArguments));
+        foreach ($spListResponse->json() as $entityId => $sp) {
+//            echo "\nChecking SP '{$entityId}'";
+            $this->execMethods($spMethods, array(
+                'entityid' => $entityId
+            ));
         }
     }
-    return $diff;
+
+    // Exec all methods for each IDP
+    public function testIdps()
+    {
+        $idpMethods = array_merge($this->genericMethods, $this->idpMethods);
+        $idpListReidponse = $this->createReidponse($this->oldHttpClient, array_merge(
+            array('method' => 'getIdpList'),
+            $this->defaultArguments));
+        foreach ($idpListReidponse->json() as $entityId => $idp) {
+            echo "\nChecking IDP '{$entityId}'";
+            $this->execMethods($idpMethods, array(
+                'entityid' => $entityId
+            ));
+        }
+    }
+
+    //array_merge($this->genericMethods,
+
+    /**
+     * @param array $methods
+     * @param array $parameters
+     */
+    private function execMethods(array $methods, array $parameters)
+    {
+        foreach ($methods as $method => $methodArguments) {
+            $arguments['method'] = $method;
+            $arguments = array_merge($arguments, $this->defaultArguments, $methodArguments);
+
+            foreach ($arguments as $argument => $argumentValue) {
+                if (isset($parameters[$argumentValue])) {
+                    $arguments[$argument] = $parameters[$argumentValue];
+                }
+            }
+
+            echo "\n - calling {$method}...old...";
+
+            try {
+                $oldResponse = $this->createResponse($this->oldHttpClient, $arguments);
+            } catch (Exception $ex) {
+                echo "Failed " . PHP_EOL .
+                    $ex->getMessage() . PHP_EOL;
+                break;
+            }
+
+            echo "new...";
+
+            try {
+                $newResponse = $this->createResponse($this->oldHttpClient, $arguments);
+            } catch (Exception $ex) {
+                echo "Failed " . PHP_EOL .
+                    $ex->getMessage() . PHP_EOL;
+                break;
+            }
+
+            echo "comparing...";
+
+            $oldJson = $oldResponse->json();
+            $newJson = $newResponse->json();
+
+            $diff = $this->array_diff_recursive($oldJson, $newJson);
+            if (!empty($diff)) {
+                var_dump($diff);
+            }
+
+            echo "done";
+        }
+    }
+
+    private function createResponse(\Guzzle\Http\Client $client, array $arguments)
+    {
+        $request = $client->get('', array(), array(
+            'query' => $arguments
+        ));
+        $request->getCurlOptions()->set(CURLOPT_SSL_VERIFYHOST, false);
+        $request->getCurlOptions()->set(CURLOPT_SSL_VERIFYPEER, false);
+
+        return $request->send();
+    }
+
+    private function array_diff_recursive($array1, $array2)
+    {
+        $diff = array();
+        foreach ($array1 as $key => $value) {
+            if (array_key_exists($key, $array2)) {
+                if (is_array($value)) {
+                    $subDiff = $this->array_diff_recursive($value, $array2[$key]);
+                    if (count($subDiff)) {
+                        $diff[$key] = $subDiff;
+                    }
+                } else {
+                    if ($value != $array2[$key]) {
+                        $diff[$key] = $value;
+                    }
+                }
+            } else {
+                $diff[$key] = $value;
+            }
+        }
+        return $diff;
+    }
 }
-
-
-echo 'end';
