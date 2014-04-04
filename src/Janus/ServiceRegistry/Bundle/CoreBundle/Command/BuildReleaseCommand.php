a4f1cddc08a53ce73891d1fbc4cefd5e1ac3bcc0
lucasvanlierop <lucas@vanlierop.org>
2014-04-04 14:24:09 +0200

    #401 [WIP] converting SR script into php

--- src/Janus/ServiceRegistry/Bundle/CoreBundle/Command/BuildReleaseCommand.php
+++ src/Janus/ServiceRegistry/Bundle/CoreBundle/Command/BuildReleaseCommand.php
@@ -43,5 +43,78 @@ class BuildReleaseCommand extends Command
     {
         $version = $input->getArgument('version');
         $output->writeln("<info>Creating a release for {$version} <info>");
+
+        #!/bin/sh
+        $releaseDir = "${HOME}/Releases";
+        $githubUser = "OpenConext";
+        $projectName = "OpenConext-serviceregistry";
+
+        $tag = $version;
+
+        $projectDirName = $(echo "{$projectName}-{$tag}" | sed - e "s/\//-/g")
+        $projectDir = {
+        $releaseDir}/ {
+        $projectDirName}
+        $janusDir = "{$projectDir}/vendor/janus-ssp/janus/"
+        
+        // Create empty dir
+        mkdir($releaseDir, 0777, true);
+        rmdir($projectDir);
+        
+        // get Composer
+        cd($releaseDir);
+        exec("curl - O http://getcomposer.org/composer.phar");
+        
+        // clone the tag
+        cd($releaseDir);
+        exec("git clone -b {$tag} https://github.com/{$githubUser}/{$projectName}.git {$projectDirName}", );
+        
+        // run Composer
+        cd("{$projectDir}
+        php {$releaseDir}/ composer . phar install--no - dev
+        
+        cd("{$janusDir}
+        
+        // remove files that are not required for production
+        rmdir("{$projectDir}/.idea");
+        rmdir("{$projectDir}/.git");
+        unlink("{$projectDir}/.gitignore");
+        unlink("{$projectDir}/ composer . json");
+        unlink("{$projectDir}/ composer . lock");
+        unlink("{$projectDir}/ makeRelease . sh");
+        unlink("{$projectDir}/ bin / composer . phar");
+        rmdir("{$projectDir}/ tests");
+        rmdir("{$projectDir}/ janus - dictionaries");
+        rmdir("{$projectDir}/ simplesamlphp_patches");
+        rmdir("{$janusDir}/ www / install");
+        
+        // create tarball
+        $releaseTarballName = {
+        $projectDirName} . tar . gz
+        $releaseTarballFile = {
+        $releaseDir}/ {
+        $releaseTarballName}
+        cd("{$releaseDir}");
+        tar - czf {
+        $releaseTarballFile} {
+        $projectDirName}
+    }
+
+    /**
+     * Executes console command.
+     *
+     * @param string $command
+     * @return mixed
+     * @throws \RuntimeException
+     */
+    private function exec($command)
+    {
+        exec($command, $output, $returnVal);
+
+        if ($returnVal != 0) {
+            throw new \RuntimeException("Command failed with code {$returnVal}");
+        }
+
+        return $output;
     }
 }
