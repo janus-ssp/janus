e590d93b858e3af0b530503c24e009f0e9920b08
lucasvanlierop <lucas@vanlierop.org>
2014-05-12 13:32:26 +0200

    #468 Added support for loading custom translations into tpl

---
+++ lib/CustomDictionaryLoader.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * Loads custom dictionaries.
+ *
+ * Class CustomDictionaryLoader
+ * @package Janus\ServiceRegistry\Bundle\SSPIntegrationBundle\Compat
+ */
+class sspmod_janus_CustomDictionaryLoader
+{
+    /**
+     * @var string
+     */
+    private $languageCode;
+
+    /**
+     * @var SimpleSAML_XHTML_Template
+     */
+    private $template;
+
+    /**
+     * @param SimpleSAML_XHTML_Template $template
+     */
+    public function __construct(SimpleSAML_XHTML_Template $template)
+    {
+        $this->template = $template;
+        $this->languageCode = $template->getLanguage();
+    }
+
+    /**
+     * @param $dir
+     */
+    public function addFromDir($dir)
+    {
+        $iterator = new DirectoryIterator($dir);
+        /** @var DirectoryIterator $file */
+        foreach ($iterator as $file) {
+            $this->addFromFile($file);
+        }
+    }
+
+    /**
+     * @param DirectoryIterator $file
+     */
+    private function addFromFile(DirectoryIterator $file)
+    {
+        $match = preg_match('/([^.]+)-custom.definition.json/', $file->getFilename(), $matches);
+        if (!$match) {
+            return;
+        }
+        $dictionaryName = $matches[1];
+
+        $translations = $this->loadTranslations($file->getPathname());
+        foreach ($translations as $translationName => $translations) {
+            $this->includeTranslation($dictionaryName, $translationName, $translations);
+        }
+    }
+
+    /**
+     * @param string $filePath
+     * @return mixed/opt/www/OpenConext-serviceregistry/simplesamlphp/modules/janus/tests/lib/CustomDictionaryLoaderTestResources/dictionaries
+     */
+    private function loadTranslations($filePath)
+    {
+        $dictonaryJson = file_get_contents($filePath);
+        $translations = json_decode($dictonaryJson, true);
+
+        return $translations;
+    }
+
+    /**
+     * @param string $dictionaryName
+     * @param string $translationName
+     * @param array $translations
+     */
+    private function includeTranslation($dictionaryName, $translationName, array $translations)
+    {
+        if (!isset($translations[$this->languageCode])) {
+            return;
+        }
+
+        $translation = $translations[$this->languageCode];
+        $this->template->includeInlineTranslation(
+            $this->createTagForTranslation($dictionaryName, $translationName),
+            $translation
+        );
+    }
+
+    /**
+     * Creates a Simplesamlphp compatible translation tag
+     *
+     * @param string $dictonaryName
+     * @param string $translationName
+     * @return string
+     */
+    private function createTagForTranslation($dictonaryName, $translationName)
+    {
+        return '{janus:' . $dictonaryName . ':' . $translationName . '}';
+    }
+}
