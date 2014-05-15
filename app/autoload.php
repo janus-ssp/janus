2357c2ebd8ab921a4b8311feb3e922001713a55a
Boy Baukema <boy@ibuildings.nl>
2014-05-15 13:59:39 +0200

    Fixed autoloading of SSP

--- app/autoload.php
+++ app/autoload.php
@@ -4,6 +4,18 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
 /**
+ * Note that this can be called in 2 ways:
+ * * Standalone, via the new REST API, in which case we need to autoload the parent SSP so we have access to
+ *   SSP utilities like SimpleSAML_Configuration that we still use.
+ * * As an include of SSPs module.php in which case SSP has already been loaded.
+ */
+define('SIMPLESAML_AUTOLOADER', __DIR__ . '/../../../lib/_autoload.php');
+
+if (!class_exists('SimpleSAML_Configuration', true) && file_exists(SIMPLESAML_AUTOLOADER)) {
+    require_once SIMPLESAML_AUTOLOADER;
+}
+
+/**
  * @var $loader ClassLoader
  */
 $loader = require __DIR__ . '/../vendor/autoload.php';
