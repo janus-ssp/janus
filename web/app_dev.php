d8bad59723940155769fdf1675ec193e7472fd40
Boy Baukema <boy@ibuildings.nl>
2014-05-15 11:15:30 +0200

    Disallow access to app_dev.php unless SFDEV is set in vhost

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,14 +8,13 @@ use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-// Disabled for now, @todo fix mod_rewrite
-//if (isset($_SERVER['HTTP_CLIENT_IP'])
-//    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-//    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
-//) {
-//    header('HTTP/1.0 403 Forbidden');
-//    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-//}
+
+// Custom: require Vhost to state that this can be used by setting:
+//     SetEnv SFDEV 1
+if (!getenv('SFDEV')) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
 
 $loader = require_once __DIR__.'/../app/autoload.php';
 require_once __DIR__.'/../app/AppKernel.php';
