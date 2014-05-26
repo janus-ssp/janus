be6d18962f9645eda81e44637cf6d4c4d99574db
lucasvanlierop <lucas@vanlierop.org>
2014-05-26 13:29:22 +0200

    #472 Converter is now a class

--- bin/convertConfigToYml.php
+++ bin/convertConfigToYml.php
@@ -2,62 +2,101 @@
 /**
  * @author Lucas van Lierop <lucas@vanlierop.org>
  */
-
 require_once __DIR__ . "/../vendor/autoload.php";
-require __DIR__ . "/../config-templates/module_janus.php";
 
-function set($path, $value, &$target)
+class ConfigToYmlConverter
 {
-    if (empty($path)) {
-        throw new InvalidArgumentException("Path should not be empty");
+    public function dump()
+    {
+        $config = $this->loadConfig();
+        $config = $this->correctAccessConfig($config);
+        $newConfig = $this->wrapConfigInNamespace($config);
+
+        $yamlDumper = new \Symfony\Component\Yaml\Dumper();
+        echo $yamlDumper->dump($newConfig, 10);
     }
 
-    if (!is_string($path)) {
-        throw new InvalidArgumentException("Path is a '" . gettype($path) . "', expected a string");
+    private function loadConfig()
+    {
+        require __DIR__ . "/../config-templates/module_janus.php";
+        return $config;
     }
 
-    $pathParts = explode(".", $path);
-    $target =& $target;
-    do {
-        $partName = array_shift($pathParts);
-
-        // Store value if path is found
-        if (empty($pathParts)) {
-            $target[$partName] = $value;
-            return;
+    /**
+     * @param array $config
+     * @return array
+     */
+    private function wrapConfigInNamespace($config)
+    {
+        $newConfig = array(
+            'janus_service_registry_core' => array()
+        );
+        foreach ($config as $key => $val) {
+            $this->set($key, $val, $newConfig['janus_service_registry_core']);
         }
 
-        // Get reference to nested child
-        if (!array_key_exists($partName, $target)) {
-            $target[$partName] = array();
-        }
-        $target =& $target[$partName];
-    } while (true);
-}
+        return $newConfig;
+    }
 
-// Wrap roles in role namespace to prevent mixing booleans and arrays
-$parsedRights = array();
-foreach ($config['access'] as $rightName => $workflowStates) {
-    foreach ($workflowStates as $state => $rights) {
-        if ($state === 'default') {
-            $parsedRights[$rightName]['default'] = $rights;
-        } else {
-            if ($state === 'role') {
-                $parsedRights[$rightName]['workflow_states']['all'] = $rights;
-            } else {
-                $parsedRights[$rightName]['workflow_states'][$state] = $rights['role'];
+    /**
+     * @param string $path
+     * @param mixed $value
+     * @param array &$target
+     * @throws InvalidArgumentException
+     */
+    private function set($path, $value, &$target)
+    {
+        if (empty($path)) {
+            throw new InvalidArgumentException("Path should not be empty");
+        }
+
+        if (!is_string($path)) {
+            throw new InvalidArgumentException("Path is a '" . gettype($path) . "', expected a string");
+        }
+
+        $pathParts = explode(".", $path);
+        $target =& $target;
+        do {
+            $partName = array_shift($pathParts);
+
+            // Store value if path is found
+            if (empty($pathParts)) {
+                $target[$partName] = $value;
+                return;
+            }
+
+            // Get reference to nested child
+            if (!array_key_exists($partName, $target)) {
+                $target[$partName] = array();
+            }
+            $target =& $target[$partName];
+        } while (true);
+    }
+
+    /**
+     * Wrap roles in role namespace to prevent mixing booleans and arrays
+     */
+    private function correctAccessConfig($config)
+    {
+        $parsedRights = array();
+        foreach ($config['access'] as $rightName => $workflowStates) {
+            foreach ($workflowStates as $state => $rights) {
+                if ($state === 'default') {
+                    $parsedRights[$rightName]['default'] = $rights;
+                } else {
+                    if ($state === 'role') {
+                        $parsedRights[$rightName]['workflow_states']['all'] = $rights;
+                    } else {
+                        $parsedRights[$rightName]['workflow_states'][$state] = $rights['role'];
+                    }
+                }
             }
         }
+        $config['access'] = $parsedRights;
+
+        return $config;
     }
 }
-$config['access'] = $parsedRights;
 
-$newConfig = array(
-    'janus_service_registry_core' => array()
-);
-foreach ($config as $key => $val) {
-    set($key, $val, $newConfig['janus_service_registry_core']);
-}
-
-$yamlDumper = new \Symfony\Component\Yaml\Dumper();
-echo $yamlDumper->dump($newConfig, 10);
+$converter = new ConfigToYmlConverter();
+$converter->dump();
