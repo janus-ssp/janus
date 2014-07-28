a3c0bb8fa9a3368b7f67aa5567131f22a6a18a49
lucasvanlierop <lucas@vanlierop.org>
2014-07-28 12:55:51 +0200

    #499 [WIP] Convert connections to dto's

--- src/Janus/ServiceRegistry/Bundle/CoreBundle/Command/DumpCommand.php
+++ src/Janus/ServiceRegistry/Bundle/CoreBundle/Command/DumpCommand.php
@@ -1,6 +1,8 @@
 <?php
 namespace Janus\ServiceRegistry\Bundle\CoreBundle\Command;
 
+use Janus\ServiceRegistry\Bundle\CoreBundle\DependencyInjection\ConfigProxy;
+use Janus\ServiceRegistry\Connection\ConnectionDtoCollection;
 use Janus\ServiceRegistry\Service\ConnectionService;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
@@ -28,5 +30,25 @@ class DumpCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln("Dumping connections");
+
+        $this->dump(
+            $this->getContainer()->get('connection_service'),
+            $this->getContainer()->get('janus_config')
+        );
+    }
+
+    /**
+     * @param ConnectionService $connectionService
+     * @param ConfigProxy $config
+     */
+    public function dump(ConnectionService $connectionService, ConfigProxy $config)
+    {
+        $connectionsRevisions = $connectionService->findLatestRevisionsWithFilters();
+
+        $connections = new ConnectionDtoCollection();
+        foreach ($connectionsRevisions as $connectionRevision) {
+            $connection = $connectionRevision->toDto($config);
+            $connections->addConnection($connection);
+        }
     }
 }
