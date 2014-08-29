77b6d34cb2244bc7b02b5ef1ada0706ea59c487a
Boy Baukema <boy@ibuildings.nl>
2014-08-29 12:21:14 +0200

    Bugfix to refactoring: Actually fix return a nested MetadataDto from the SimpleAssembler

--- src/Janus/ServiceRegistry/Connection/Metadata/MetadataDto/Assembler/CastingAssembler.php
+++ src/Janus/ServiceRegistry/Connection/Metadata/MetadataDto/Assembler/CastingAssembler.php
@@ -3,7 +3,6 @@
 namespace Janus\ServiceRegistry\Connection\Metadata\MetadataDto\Assembler;
 
 use Doctrine\Common\Collections\Collection;
-use Janus\ServiceRegistry\ArrayPathHelper;
 use Janus\ServiceRegistry\Connection\Metadata\MetadataDto;
 use Janus\ServiceRegistry\Connection\Metadata\MetadataDto\MetadataDefinitionHelper;
 
@@ -30,27 +29,9 @@ class CastingAssembler extends SimpleAssembler
      */
     public function assemble(Collection $metadata)
     {
-        return $this->createFromFlatArray(
-            parent::assemble($metadata)
-        );
-    }
-
-    /**
-     * Turns a flat array into a MetadataDto.
-     *
-     * @param array $flatCollection
-     * @return MetadataDto
-     */
-    private function createFromFlatArray(array $flatCollection)
-    {
-        $flatCollection = $this->metadataDefinitionHelper->castData($flatCollection);
-
-        $arrayPathHelper = new ArrayPathHelper();
-        foreach ($flatCollection as $key => $value) {
-            $arrayPathHelper->set($key, $value);
-        }
-        $items = $arrayPathHelper->getArray();
-
-        return new MetadataDto($items);
+        $flatMetadata = $this->collectionToArray($metadata);
+        $castedMetadata = $this->metadataDefinitionHelper->castData($flatMetadata);
+        $nestedMetadata = $this->nestMetadata($castedMetadata);
+        return $nestedMetadata;
     }
 }
--- src/Janus/ServiceRegistry/Connection/Metadata/MetadataDto/Assembler/SimpleAssembler.php
+++ src/Janus/ServiceRegistry/Connection/Metadata/MetadataDto/Assembler/SimpleAssembler.php
@@ -3,17 +3,37 @@
 namespace Janus\ServiceRegistry\Connection\Metadata\MetadataDto\Assembler;
 
 use Doctrine\Common\Collections\Collection;
+use Janus\ServiceRegistry\ArrayPathHelper;
+use Janus\ServiceRegistry\Connection\Metadata\MetadataDto;
 use Janus\ServiceRegistry\Entity\Connection\Revision\Metadata;
 
 class SimpleAssembler implements AssemblerInterface
 {
     public function assemble(Collection $metadata)
     {
-        /** @var Metadata $metadataRecord */
+        $flatMetadata   = $this->collectionToArray($metadata);
+        $nestedMetadata = $this->nestMetadata($flatMetadata);
+        return $nestedMetadata;
+    }
+
+    protected function collectionToArray(Collection $metadata)
+    {
         $flatMetadata = array();
         foreach ($metadata as $metadataRecord) {
+            /** @var Metadata $metadataRecord */
             $flatMetadata[$metadataRecord->getKey()] = $metadataRecord->getValue();
         }
         return $flatMetadata;
+    }
+
+    protected function nestMetadata($flatMetadata)
+    {
+        $arrayPathHelper = new ArrayPathHelper();
+        foreach ($flatMetadata as $key => $value) {
+            $arrayPathHelper->set($key, $value);
+        }
+        $items = $arrayPathHelper->getArray();
+
+        return new MetadataDto($items);
     }
 }
