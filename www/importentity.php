1727e6125cc645b858e34b89d8c6da3770e4f99f
Tim van Dijen <tvdijen@gmail.com>
2016-09-22 11:25:08 +0200

    Update importentity.php

--- www/importentity.php
+++ www/importentity.php
@@ -23,6 +23,8 @@ $authenticationSource = $janusConfig->getValue('auth', 'login-admin');
 /** @var $userIdAttribute string */
 $userIdAttribute = $janusConfig->getValue('useridattr', 'eduPersonPrincipalName');
 
+$as = new SimpleSAML_Auth_Simple($authsource);
+
 // Validate user
 if ($as->isAuthenticated($authenticationSource)) {
     $attributes = $as->getAttributes();
