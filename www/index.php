janus-ssp/janus

commit 022d072840fb79bda899a1b88dd77040393bf3e4

Fixes SimpleSAML_Session::getInstance() deprecation in 1.14

www/index.php
@@ -5,9 +5,12 @@
 
 require __DIR__ . '/_includes.php';
 
-$session = SimpleSAML_Session::getInstance();
 $config = SimpleSAML_Configuration::getInstance();
 $janus_config = sspmod_janus_DiContainer::getInstance()->getConfig();
+$authsource = $janus_config->getValue('auth', 'login-admin');
+$useridattr = $janus_config->getValue('useridattr', 'eduPersonPrincipalName');
+
+$as = new SimpleSAML_Auth_Simple($authsource);
 
 // Error loggin in has happend
 if(isset($_GET['error'])) {
@@ -23,31 +26,15 @@ if(isset($_GET['error'])) {
     exit();
 }
 
-$authsource = $janus_config->getValue('auth', 'login-admin');
-$useridattr = $janus_config->getValue('useridattr', 'eduPersonPrincipalName');
 
-if ($session->isValid($authsource)) {
-    $attributes = $session->getAttributes();
-    // Check if userid exists
-    if (!isset($attributes[$useridattr]))
-        throw new Exception('User ID is missing');
-    $userid = $attributes[$useridattr][0];
-} else {
-    $returnURL = $session->getData('string', 'refURL');
 
-    if (is_null($returnURL)) {
-        $returnURL = SimpleSAML_Utilities::selfURL();
-    } else {
-        $session->deleteData('string' ,'refURL');
-    }
-    
-    SimpleSAML_Auth_Default::initLogin(
-        $authsource,
-        $returnURL,
-        NULL,
-        $_GET
-    );
-}
+if (!$as->isAuthenticated()) $as->requireAuth();
+
+$attributes = $as->getAttributes();
+// Check if userid exists
+if (!isset($attributes[$useridattr]))
+    throw new Exception('User ID is missing');
+$userid = $attributes[$useridattr][0];
 
 $user = new sspmod_janus_User();
 $user->setUserid($userid);

src/Janus/ServiceRegistry/Security/Authentication/Provider/SspProvider.php
@@ -37,8 +37,8 @@ class SspProvider implements AuthenticationProviderInterface
             return $this->getTokenForUsername($authenticationType);
         }
 
-        $session = \SimpleSAML_Session::getInstance();
-        if (!$session->isValid($authenticationType)) {
+        $as = new \SimpleSAML_Auth_Simple($authenticationType);
+        if (!$as->isAuthenticated()) {
             throw new AuthenticationException("Authsource '$authenticationType' is invalid");
         }
 
@@ -46,7 +46,7 @@ class SspProvider implements AuthenticationProviderInterface
         $userIdAttributeName = $this->config->getValue('useridattr', 'eduPersonPrincipalName');
 
         // Check if userid exists
-        $attributes = $session->getAttributes();
+        $attributes = $as->getAttributes();
         if (!isset($attributes[$userIdAttributeName])) {
             throw new AuthenticationException("Attribute '$userIdAttributeName' with User ID is missing.");
         }

www/AJAXRequestHandler.php
@@ -4,11 +4,12 @@ use Janus\ServiceRegistry\Bundle\CoreBundle\DependencyInjection\ConfigProxy;
 
 require __DIR__ . '/_includes.php';
 
-$session = SimpleSAML_Session::getInstance();
 $janus_config = sspmod_janus_DiContainer::getInstance()->getConfig();
-
 $authsource = $janus_config->getValue('auth', 'login-admin');
-if (!$session->isValid($authsource)) {
+
+$as = new SimpleSAML_Auth_Simple($authsource);
+
+if (!$as->isAuthenticated()) {
     echo json_encode(array("status" => "error_no_session"));
     throw new SimpleSAML_Error_Exception('No valid session');
 }
@@ -58,7 +59,7 @@ if (!isset($_POST['csrf_token']) || !$csrf_provider->isCsrfTokenValid('ajax', $_
     die(json_encode(array('status'=>'error_csrf')));
 }
 
-$user = getUser($session, $janus_config);
+$user = getUser($as, $janus_config);
 $securityContext = sspmod_janus_DiContainer::getInstance()->getSecurityContext();
 
 // ??? is 'allentities' the right permission for enabling superuser status ???
@@ -106,14 +107,14 @@ if ($return) {
 echo json_encode($result);
 
 
-function getUser(SimpleSAML_Session $session, ConfigProxy $janus_config)
+function getUser(SimpleSAML_Auth_Simple $as, ConfigProxy $janus_config)
 {
     // Get data from config
     /** @var string $useridattr */
     $useridattr = $janus_config->getValue('useridattr', 'eduPersonPrincipalName');
 
     // Validate user
-    $attributes = $session->getAttributes();
+    $attributes = $as->getAttributes();
 
     // Check if userid exists
     if (!isset($attributes[$useridattr])) {

www/dashboard.php
@@ -31,13 +31,15 @@ define('TAB_AJAX_CONTENT_PREFIX', 'ajax-content/');
 require __DIR__ . '/_includes.php';
 
 set_time_limit(180);
-$session = SimpleSAML_Session::getInstance();
+$session = SimpleSAML_Session::getSession();
 $config = SimpleSAML_Configuration::getInstance();
 $janus_config = sspmod_janus_DiContainer::getInstance()->getConfig();
 $csrf_provider = sspmod_janus_DiContainer::getInstance()->getCsrfProvider();
 
 $authsource = $janus_config->getValue('auth', 'login-admin');
 $useridattr = $janus_config->getValue('useridattr', 'eduPersonPrincipalName');
+
+$as = new SimpleSAML_Auth_Simple($authsource);
 
 // Note: $param variable is provided by SimpleSaml but only if there actually is a 'param' part in the url
 if (!isset($param)) {
@@ -53,14 +55,14 @@ if (current($tabPath) . '/' === TAB_AJAX_CONTENT_PREFIX) {
 define('IS_AJAX', $isAjax);
 
 // Validate user
-if ($session->isValid($authsource)) {
-    $attributes = $session->getAttributes();
+if ($as->isAuthenticated()) {
+    $attributes = $as->getAttributes();
     // Check if userid exists
     if (!isset($attributes[$useridattr]))
         throw new Exception('User ID is missing');
     $userid = $attributes[$useridattr][0];
 } else {
-    redirectTrustedUrl(SimpleSAML_Module::getModuleURL('janus/index.php'), $_GET, IS_AJAX);
+    $as->requireAuth();
 }
 
 function check_uri ($uri)
@@ -363,7 +365,7 @@ $template->data['last_page'] = ceil((float)$messages_total / $pm->getPaginationC
 
 
 
-$template->data['logouturl'] = SimpleSAML_Module::getModuleURL('core/authenticate.php') . '?logout=1&as=' . urlencode($session->getAuthority());
+$template->data['logouturl'] = $as->getLogoutURL();
 
 
 /* START TAB ARPADMIN PROVISIONING ************************************************************************************/

www/editentity.php
@@ -9,7 +9,7 @@ require __DIR__ . '/_includes.php';
 // Initial import
 /** @var $session SimpleSAML_Session */
 set_time_limit(180);
-$session = SimpleSAML_Session::getInstance();
+$session = SimpleSAML_Session::getSession();
 $config = SimpleSAML_Configuration::getInstance();
 $janus_config = sspmod_janus_DiContainer::getInstance()->getConfig();
 

www/exportentity.php
@@ -10,15 +10,17 @@
 require __DIR__ . '/_includes.php';
 
 /* Load simpleSAMLphp, configuration and metadata */
-$session = SimpleSAML_Session::getInstance();
+$session = SimpleSAML_Session::getSession();
 $config = SimpleSAML_Configuration::getInstance();
 $janus_config = sspmod_janus_DiContainer::getInstance()->getConfig();
 
 $authsource = $janus_config->getValue('auth', 'login-admin');
 $useridattr = $janus_config->getValue('useridattr', 'eduPersonPrincipalName');
 
-if ($session->isValid($authsource)) {
-    $attributes = $session->getAttributes();
+$as = new SimpleSAML_Auth_Simple($authsource);
+
+if ($as->isAuthenticated()) {
+    $attributes = $as->getAttributes();
     // Check if userid exists
     if (!isset($attributes[$useridattr]))
         throw new Exception('User ID is missing');

www/history.php
@@ -17,7 +17,6 @@
 require __DIR__ . '/_includes.php';
 
 // Initial setup
-$session      = SimpleSAML_Session::getInstance();
 $config       = SimpleSAML_Configuration::getInstance();
 $janus_config = sspmod_janus_DiContainer::getInstance()->getConfig();
 $authsource   = $janus_config->getValue('auth', 'login-admin');
@@ -28,9 +27,11 @@ $et           = new SimpleSAML_XHTML_Template(
     'janus:editentity'
 );
 
+$as = new SimpleSAML_Auth_Simple($authsource);
+
 // Validate user
-if ($session->isValid($authsource)) {
-    $attributes = $session->getAttributes();
+if ($as->isAuthenticated()) {
+    $attributes = $as->getAttributes();
     // Check if userid exists
     if (!isset($attributes[$useridattr])) {
         throw new Exception('User ID is missing');

www/importentity.php
@@ -23,9 +23,11 @@ $authenticationSource = $janusConfig->getValue('auth', 'login-admin');
 /** @var $userIdAttribute string */
 $userIdAttribute = $janusConfig->getValue('useridattr', 'eduPersonPrincipalName');
 
+$as = new SimpleSAML_Auth_Simple($authsource);
+
 // Validate user
-if ($session->isValid($authenticationSource)) {
-    $attributes = $session->getAttributes();
+if ($as->isAuthenticated()) {
+    $attributes = $as->getAttributes();
     // Check if user id exists
     if (!isset($attributes[$userIdAttribute])) {
         throw new Exception('User ID is missing');
@@ -228,6 +230,7 @@ $et->show();
 
 function janus_array_diff_recursive($array1, $array2)
 {
+    if (!is_array($array2)) return $array1;
     $diff = array();
     foreach ($array1 as $key => $value) {
         if (is_array($array2) && array_key_exists($key, $array2)) {

commit f4da88a48a9bbd08b323b6c983e2cd071dcd250f

bugfix: inactive user would get fatal error instead of nice message
broken in 022d072840fb79bda899a1b88dd77040393bf3e4

www/index.php
@@ -56,10 +56,10 @@ if(!$user->load(sspmod_janus_User::USERID_LOAD)) {
             SimpleSAML_Module::getModuleURL('janus/dashboard.php/entities')
         );
     } else {
-        $session->doLogout();
+        $session = SimpleSAML_Session::getSessionFromRequest();
+        $session->doLogout($authsource);
         SimpleSAML_Utilities::redirectTrustedUrl(
             SimpleSAML_Module::getModuleURL('janus/index.php?error=error_index_user_inactive')
         );
     }
 }
-?>